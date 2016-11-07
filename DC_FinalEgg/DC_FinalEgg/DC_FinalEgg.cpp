f7d14a5c2fee9090421034d701e938b32da5b9af
PkR <pkr@sadx>
2016-11-08 08:37:48 +0900

    Final Egg Acts 2 and 3

--- DC_FinalEgg/DC_FinalEgg/DC_FinalEgg.cpp
+++ DC_FinalEgg/DC_FinalEgg/DC_FinalEgg.cpp
@@ -1,14 +1,24 @@
 #include "stdafx.h"
 #include "SADXModLoader.h"
 #include "FinalEgg1.h"
+#include "FinalEgg2.h"
+#include "FinalEgg3.h"
 
 PointerInfo pointers[] = {
-	ptrdecl(0x97DB48, &landtable_0001D108)
+	ptrdecl(0x97DB48, &landtable_0001D108),
+	ptrdecl(0x97DB4C, &landtable_00083CCC),
+	ptrdecl(0x97DB50, &landtable_000E67D0)
 };
 
-extern "C" __declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
 
-extern "C" __declspec(dllexport) const ModInfo SADXModInfo = { ModLoaderVer };
-
-
+extern "C"
+{
+	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
+	__declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
+	__declspec(dllexport) void __cdecl OnFrame()
+	{
+		Uint8 FogColor[4] = { 0x00u, 0x00u, 0x00u, 0x00u };
+		if (CurrentLevel == 10) WriteData((void*)0x03ABDC68, (void*)FogColor, sizeof(char) * 4);
+	}
+}
 
--- DC_Fog/DC_Fog/DC_Fog.cpp
+++ DC_Fog/DC_Fog/DC_Fog.cpp
@@ -7,15 +7,7 @@ extern "C"
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
 	__declspec(dllexport) void __cdecl OnFrame()
 	{
-		void PreviousLevel(int& CurrentLevel);
-		void PreviousAct(int& CurrentAct);
-		if (PreviousLevel != CurrentLevel || PreviousAct != CurrentAct)
-		{
 			Uint8 FogColor[4] = { 0x00u, 0x00u, 0x00u, 0x00u };
 			if (CurrentLevel == 10, CurrentAct == 0) WriteData((void*)0x03ABDC68, (void*)FogColor, sizeof(char) * 4);
-			if (CurrentLevel == 4, CurrentAct == 2) WriteData((void*)0x03ABDC68, (void*)FogColor, sizeof(char) * 4);
-			PreviousLevel == CurrentLevel;
-			PreviousAct == CurrentAct;
-		}
 	}
 }
