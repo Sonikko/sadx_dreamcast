c65f79bbfb281fdce930a5a8e4d3b0d0c0859974
PkR <pkr@sadx>
2016-11-26 00:09:06 +0900

    Red Mountain skull test, DC Bosses update with Perfect Chaos and more animlist hooks

--- DC_Bosses/DC_Bosses/DC_Bosses.cpp
+++ DC_Bosses/DC_Bosses/DC_Bosses.cpp
@@ -4,19 +4,57 @@
 #include "EggViper.h"
 #include "Chaos2.h"
 #include "Chaos4.h"
-#include "Chaos6_Act1.h"
 #include "Chaos6_Act1_PC.h"
 #include "Chaos6_Act2.h"
+#include "Chaos7.h"
+#include "E101.h"
+#include "Zero.h"
+#include "Zero_PC.h"
+#include "EggHornet.h"
+
 
 PointerInfo pointers[] = {
-	ptrdecl(0x7D1CC0, &landtable_00D2136C), //Chaos 2
+	//ptrdecl(0x7D1CC0, &landtable_0000028C), //Chaos 2
 	ptrdecl(0x7D1CD6, &landtable_00000238), //Chaos 4
-	ptrdecl(0x7D1CEC, &landtable_00DEDE38), //Chaos 6 Act 1
 	ptrdecl(0x7D1CF6, &landtable_0000033C), //Chaos 6 Act 2
-	ptrdecl(0x7D1D32, &landtable_0000022C), // Egg Walker
-	ptrdecl(0x7D1D48, &landtable_000580F4), // Egg Viper
+	ptrdecl(0x7D1D1C, &landtable_00000128), //Egg Hornet
+	ptrdecl(0x7D1D32, &landtable_0000022C), //Egg Walker
+	ptrdecl(0x7D1D48, &landtable_000580F4), //Egg Viper
+	ptrdecl(0x7D1D7F, &landtable_00000068), //E-101
+	ptrdecl(0x7D1D06, &landtable_00001214), //Perfect Chaos DC
+//	ptrdecl(0x7D1D06, &landtable_0102478C), //Perfect Chaos PC
 };
 
-extern "C" __declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
+extern "C"
+{
+	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
+	__declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
+	__declspec(dllexport) void __cdecl Init()
+	{
+		ResizeTextureList((NJS_TEXLIST*)0x1557064, textures_egm1land);
+		ResizeTextureList((NJS_TEXLIST*)0x1494FBC, textures_chaos7);
+		memcpy((void*)0x011EDE20, &animlist_00DEDE20, sizeof(animlist_00DEDE20)); // Chaos6 animlist
+		memcpy((void*)0x016B4D20, &animlist_012B4D20, sizeof(animlist_012B4D20)); // Zero animlist
+		memcpy((void*)0x016E3994, &object_0001C9EC, sizeof(object_0001C9EC));  // Zero 1 
+		memcpy((void*)0x01550FD4, &object_0001C9EC, sizeof(object_0001C9EC));  // E101MK2 1
+		memcpy((void*)0x016E3CCC, &object_0001CD18, sizeof(object_0001CD18));  // Zero 2 
+		memcpy((void*)0x0155130C, &object_0001CD18, sizeof(object_0001CD18));  // E101MK2 2
+		memcpy((void*)0x016E301C, &object_0001C090, sizeof(object_0001C090));  // Zero 3
+		memcpy((void*)0x0155065C, &object_0001C090, sizeof(object_0001C090));  // E101MK2 3
+		memcpy((void*)0x016E3354, &object_0001C3BC, sizeof(object_0001C3BC));  // Zero 4
+		memcpy((void*)0x01550994, &object_0001C3BC, sizeof(object_0001C3BC));  // E101MK2 
+		memcpy((void*)0x01183F04, &object_0006B438, sizeof(object_0006B438)); // Chaos2 1 
+		memcpy((void*)0x0118440C, &object_0006B890, sizeof(object_0006B890)); // Chaos2 2
+		object_0006B890.basicdxmodel->mats[0].attr_texId = 11; //Chaos2 BG object fixes
+		object_0006B438.basicdxmodel->mats[0].attr_texId = 0; //Chaos2 BG object fixes
+		DataArray(FogData, Chaos2Fog, 0x00D205DD, 3);
+		for (int i = 0; i < 3; i++)
+		{
+			Chaos2Fog[i].Color = 0xFF000000;
+			Chaos2Fog[i].Layer = 700.0f;
+			Chaos2Fog[i].Distance = 1700.0f;
+			Chaos2Fog[i].Toggle = 0;
+		}
+	}
 
-extern "C" __declspec(dllexport) const ModInfo SADXModInfo = { ModLoaderVer };
+}
--- DC_RedMountain/DC_RedMountain/DC_RedMountain.cpp
+++ DC_RedMountain/DC_RedMountain/DC_RedMountain.cpp
@@ -3,6 +3,7 @@
 #include "Mountain1.h"
 #include "Mountain2.h"
 #include "Mountain3.h"
+#include "Skull.h"
 
 PointerInfo pointers[] = {
 	ptrdecl(0x97DAA8, &landtable_00018CB8),
@@ -10,6 +11,13 @@ PointerInfo pointers[] = {
 	ptrdecl(0x97DAB0, &landtable_0001E358)
 };
 
-extern "C" __declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
+extern "C"
+{
+	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
+	__declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
+	__declspec(dllexport) void __cdecl Init()
+	{
+		memcpy((void*)0x024364C0, &attach_0014E8FC, sizeof(attach_0014E8FC));  // Skull
+	}
 
-extern "C" __declspec(dllexport) const ModInfo SADXModInfo = { ModLoaderVer };
+}
