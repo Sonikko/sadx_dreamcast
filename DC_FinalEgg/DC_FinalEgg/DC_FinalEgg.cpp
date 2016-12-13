07acfae67f25aedbe01db89b9bb117601be3d099
PkR <pkr@sadx>
2016-12-13 21:24:42 +0900

    Final Egg texlist fix

--- DC_FinalEgg/DC_FinalEgg/DC_FinalEgg.cpp
+++ DC_FinalEgg/DC_FinalEgg/DC_FinalEgg.cpp
@@ -36,14 +36,11 @@ extern "C"
 	__declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
 	__declspec(dllexport) void __cdecl Init()
 	{
-		ResizeTextureList((NJS_TEXLIST*)0x19CC1C0, 183);
 		((LandTable *)0x19C8ED0)->COLCount = LengthOfArray(collist_015C7D60);
 		((LandTable *)0x19C8ED0)->COLList = collist_015C7D60;
 		WriteJump((void*)0x5ADC40, SetClip_FEgg2_r);
-		//*(NJS_OBJECT*)0x1A4583C = object_001EDFBC; //cylinder
-		//WriteData((void*)0x005B47A1, 0x90i8, 5); // cylinder texlist
-		//WriteData((void*)0x005B475B, 0x90i8, 3); //kill cylinder animation
 		DataArray(FogData, FinalEgg1Fog, 0x019C8FF0, 3);
+		DataArray(FogData, FinalEgg2Fog, 0x019C9020, 3);
 		for (int i = 0; i < 3; i++)
 		{
 			FinalEgg1Fog[i].Color = 0xFF000000;
@@ -53,17 +50,13 @@ extern "C"
 			FinalEgg1Fog[i].Layer = 1200.0f;
 			FinalEgg1Fog[i].Distance = 3000.0f;
 			}
-		}
-		DataArray(FogData, FinalEgg2Fog, 0x019C9020, 3);
-		for (int i = 0; i < 3; i++)
-		{
 			FinalEgg2Fog[i].Color = 0xFF000000;
 			FinalEgg2Fog[i].Layer = 650.0f;
 			FinalEgg2Fog[i].Distance = 2000.0f;
 			FinalEgg2Fog[i].Toggle = 1;
 		}
 		memcpy((void*)0x19FEFE4, &object_001AEDFC, sizeof(object_001AEDFC));  // Light
-		memcpy((void*)0x19D8BC0, &attach_015D8BC0, sizeof(attach_015D8BC0));  // Lazzer!
+		memcpy((void*)0x19D8BC0, &attach_015D8BC0, sizeof(attach_015D8BC0));  // Laser
 		memcpy(&object_016C3004, &object_000E0558Z, sizeof(object_000E0558Z));  // BG 1
 		memcpy(&object_016C2FD0, &object_000E058CZ, sizeof(object_000E058CZ));  // BG 2
 		memcpy(&object_016C3038, &object_000E0524Z, sizeof(object_000E0524Z));  // BG 3
@@ -76,27 +69,4 @@ extern "C"
 		memcpy(&matlist_01695858, &matlist_01695858Z, sizeof(matlist_01695858Z));  // Trans 4
 		memcpy(&matlist_01695D50, &matlist_01695D50Z, sizeof(matlist_01695D50Z));  // Trans 5
 	}
-	/*__declspec(dllexport) void __cdecl OnFrame()
-	{
-		if (CurrentLevel == 10 && GameState == 15)
-		{
-			if (LevelFrameCount % 41 == 1)	matlist_001EDDF0[0].attr_texId = 176;
-			if (LevelFrameCount % 41 == 4)	matlist_001EDDF0[0].attr_texId = 177;
-			if (LevelFrameCount % 41 == 7)	matlist_001EDDF0[0].attr_texId = 178;
-		}
-	}*/
 }
-
-/*BK
-memcpy((void*)0x1AC3004, &object_000E0558, sizeof(object_000E0558));  // BG 1
-memcpy((void*)0x1AC2FD0, &object_000E058C, sizeof(object_000E058C));  // BG 2
-memcpy((void*)0x1AC3038, &object_000E0524, sizeof(object_000E0524));  // BG 3
-memcpy((void*)0x1A7B47C, &object_000C6218, sizeof(object_000C6218));  // BG 4
-memcpy((void*)0x1AC2F68, &object_000E04F0, sizeof(object_000E04F0));  // BG 5
-memcpy((void*)0x1AC2F9C, &object_000E05C0, sizeof(object_000E05C0));  // BG 6
-memcpy((void*)0x1AC1E04, &matlist_016C1E04Z, sizeof(matlist_016C1E04Z));  // Trans 1
-memcpy((void*)0x1ABE6A8, &matlist_016BE6A8Z, sizeof(matlist_016BE6A8Z));  // Trans 2
-memcpy((void*)0x1AC01B0, &matlist_016C01B0Z, sizeof(matlist_016C01B0Z));  // Trans 3
-memcpy((void*)0x1A95858, &matlist_01695858Z, sizeof(matlist_01695858Z));  // Trans 4
-memcpy((void*)0x1A95D50, &matlist_01695D50Z, sizeof(matlist_01695D50Z));  // Trans 5
-*/
