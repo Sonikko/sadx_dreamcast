efb05b0d7ae10b6d21c01071fd10a7906384e98c
PkR <pkr@sadx>
2016-12-27 01:06:31 +0900

    DC-like fog for Windy2

--- DC_WindyValley/DC_WindyValley/DC_WindyValley.cpp
+++ DC_WindyValley/DC_WindyValley/DC_WindyValley.cpp
@@ -1,11 +1,10 @@
-// DC_WindyValley.cpp : Defines the exported functions for the DLL application.
-//
-
 #include "stdafx.h"
 #include "SADXModLoader.h"
 #include "windy1.h"
 #include "windy2.h"
 #include "windy3.h"
+
+DataArray(FogData, FogData_Windy2, 0x00AFEA50, 3);
 
 PointerInfo pointers[] = {
 	ptrdecl(0x97DA48, &landtable_0000D7E0),
@@ -17,6 +16,13 @@ extern "C" __declspec(dllexport) const PointerList Pointers = { arrayptrandlengt
 
 extern "C" __declspec(dllexport) void cdecl Init()
 {
+	for (int i = 0; i < 3; i++)
+	{
+		FogData_Windy2[i].Color = 0xFFFFFFFF;
+		FogData_Windy2[i].Distance = 2500.0f;
+		FogData_Windy2[i].Layer = 50.0f;
+		FogData_Windy2[i].Toggle = 1;
+	}
 	ResizeTextureList((NJS_TEXLIST *)0xB98BF8, textures_windy1);
 	ResizeTextureList((NJS_TEXLIST *)0xB81304, textures_windy2);
 	ResizeTextureList((NJS_TEXLIST *)0xAFEC30, textures_windy3);
