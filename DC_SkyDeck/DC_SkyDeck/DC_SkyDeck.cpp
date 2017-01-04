9951534701e83308746c802832c632cd20ab834b
PkR <pkr@sadx>
2017-01-04 09:44:50 +0900

    Sky Deck rocks alpha fixes

--- DC_SkyDeck/DC_SkyDeck/DC_SkyDeck.cpp
+++ DC_SkyDeck/DC_SkyDeck/DC_SkyDeck.cpp
@@ -12,14 +12,6 @@ PointerInfo pointers[] = {
 
 extern "C" __declspec(dllexport) void cdecl Init()
 {
-	/*((NJS_OBJECT *)0x21FE8EC)->basicdxmodel->mats[0].attrflags |= NJD_FLAG_IGNORE_LIGHT; //Decal
-	((NJS_OBJECT *)0x21FE9F8)->basicdxmodel->mats[0].attrflags |= NJD_FLAG_IGNORE_LIGHT; //Decal
-	((NJS_OBJECT *)0x21FEB04)->basicdxmodel->mats[0].attrflags |= NJD_FLAG_IGNORE_LIGHT; //Decal
-	((NJS_OBJECT *)0x21FEC10)->basicdxmodel->mats[0].attrflags |= NJD_FLAG_IGNORE_LIGHT; //Decal
-	((NJS_OBJECT *)0x21FED1C)->basicdxmodel->mats[0].attrflags |= NJD_FLAG_IGNORE_LIGHT; //Decal
-	((NJS_OBJECT *)0x21FEE28)->basicdxmodel->mats[0].attrflags |= NJD_FLAG_IGNORE_LIGHT; //Decal
-	((NJS_OBJECT *)0x21FEF34)->basicdxmodel->mats[0].attrflags |= NJD_FLAG_IGNORE_LIGHT; //Decal
-	((NJS_OBJECT *)0x21FF040)->basicdxmodel->mats[0].attrflags |= NJD_FLAG_IGNORE_LIGHT; //Decal*/
 	ResizeTextureList((NJS_TEXLIST*)0x20E0BB0, textures_skydeck1);
 	//ResizeTextureList((NJS_TEXLIST*)0x20AA63C, textures_skydeck2);
 	ResizeTextureList((NJS_TEXLIST*)0x203ACE0, textures_skydeck3);
@@ -32,8 +24,8 @@ extern "C" __declspec(dllexport) void cdecl Init()
 		SkyDeck1Fog[i].Distance = 8000.0f;
 		SkyDeck2Fog[i].Layer = 2500.0f;
 		SkyDeck2Fog[i].Distance = 6000.0f;
-		SkyDeck3Fog[i].Layer = 4000.0f;
-		SkyDeck3Fog[i].Distance = 12000.0f;
+		//SkyDeck3Fog[i].Layer = 4000.0f;
+		//SkyDeck3Fog[i].Distance = 12000.0f;
 	}
 }
 
