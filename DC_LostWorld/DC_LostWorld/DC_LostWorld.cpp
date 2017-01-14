2b171e6df910d44fe5eeb00e24e82b5ef28822f0
PiKeyAr <pikeyar@sadx>
2017-01-15 04:14:22 +0900

    Lost World accuracy update: objects, Act 3 fog, animation optimizations

--- DC_LostWorld/DC_LostWorld/DC_LostWorld.cpp
+++ DC_LostWorld/DC_LostWorld/DC_LostWorld.cpp
@@ -3,6 +3,10 @@
 #include "LostWorld1.h"
 #include "LostWorld2.h"
 #include "LostWorld3.h"
+#include "LostWorld_objects.h"
+
+static int animw1 = 44;
+static int animw2 = 81;
 
 PointerInfo pointers[] = {
 	ptrdecl(0x97DAE8, &landtable_0000D560),
@@ -16,12 +20,20 @@ extern "C"
 	__declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
 	__declspec(dllexport) void __cdecl Init()
 	{
+		*(NJS_OBJECT*)0x1FFE9A4 = object_0012BB88; //Ashiba01
+		*(NJS_OBJECT*)0x1FFF454 = object_0012C218; //Ashiba02
+		*(NJS_OBJECT*)0x1FFFF04 = object_0012C8B4; //Ashiba03
+		*(NJS_OBJECT*)0x2023C08 = object_00148264; //Snake head
+		*(NJS_OBJECT*)0x20253BC = object_00149374; //Snake joints
+		*(NJS_MODEL_SADX*)0x2026E38 = attach_00149E7C; //Fire obstacle
+		((NJS_OBJECT*)0x2031810)->basicdxmodel->mats[0].attrflags |= NJD_DA_ONE; //Ceiling light
 		WriteData((void*)0x814CB4, 0xc1c80000, 4); //LW2 fog stuff
 		ResizeTextureList((NJS_TEXLIST*)0x1F6F02C, textures_lw1);
 		ResizeTextureList((NJS_TEXLIST*)0x1E9B9AC, textures_lw2);
 		ResizeTextureList((NJS_TEXLIST*)0x1E79D80, textures_lw3);
-		DataArray(FogData, LostWorld1Fog, 0x01E79AAC, 1);
-		DataArray(FogData, LostWorld2Fog, 0x01E79ADC, 1);
+		DataArray(FogData, LostWorld1Fog, 0x01E79AAC, 3);
+		DataArray(FogData, LostWorld2Fog, 0x01E79ADC, 3);
+		DataArray(FogData, LostWorld3Fog, 0x01E79B0C, 3);
 		DataArray(DrawDistance, DrawDist_LostWorld2, 0x01E79A7C, 3);
 		for (int i = 0; i < 3; i++)
 		{
@@ -33,272 +45,36 @@ extern "C"
 			LostWorld2Fog[i].Layer = 150.0f;
 			LostWorld2Fog[i].Distance = 3200.0f;
 			LostWorld2Fog[i].Toggle = 1;
+			LostWorld3Fog[i].Layer = 500.0f;
+			LostWorld3Fog[i].Distance = 2500.0f;
+			LostWorld3Fog[i].Color = 0xFFFFFFFF;
 			DrawDist_LostWorld2[i].Maximum = -2700.0;
 		}
 	}
 	__declspec(dllexport) void __cdecl OnFrame()
+	{
+		if (CurrentLevel == 7 && CurrentAct == 0 && GameState != 16)
 		{
-
-			{
-				if (CurrentLevel == 7 && CurrentAct == 1 && GameState != 16)
-				{
-					if (LevelFrameCount % 41 == 1)
-					{
-						matlist_000E924C[0].attr_texId = 81;
-						matlist_000E3610[0].attr_texId = 81;
-						matlist_000E8A20[0].attr_texId = 81;
-						matlist_000E43D0[0].attr_texId = 81;
-						matlist_000E7228[0].attr_texId = 81;
-						matlist_000EF078[0].attr_texId = 81;
-						matlist_000ECF80[0].attr_texId = 81;
-						matlist_000EABDC[0].attr_texId = 81;
-						matlist_000EBB3C[0].attr_texId = 81;
-					}
-					if (LevelFrameCount % 41 == 4)
-					{
-						matlist_000E924C[0].attr_texId = 82;
-						matlist_000E3610[0].attr_texId = 82;
-						matlist_000E8A20[0].attr_texId = 82;
-						matlist_000E43D0[0].attr_texId = 82;
-						matlist_000E7228[0].attr_texId = 82;
-						matlist_000EF078[0].attr_texId = 82;
-						matlist_000ECF80[0].attr_texId = 82;
-						matlist_000EABDC[0].attr_texId = 82;
-						matlist_000EBB3C[0].attr_texId = 82;
-					}
-					if (LevelFrameCount % 41 == 7)
-					{
-						matlist_000E924C[0].attr_texId = 83;
-						matlist_000E3610[0].attr_texId = 83;
-						matlist_000E8A20[0].attr_texId = 83;
-						matlist_000E43D0[0].attr_texId = 83;
-						matlist_000E7228[0].attr_texId = 83;
-						matlist_000EF078[0].attr_texId = 83;
-						matlist_000ECF80[0].attr_texId = 83;
-						matlist_000EABDC[0].attr_texId = 83;
-						matlist_000EBB3C[0].attr_texId = 83;
-					}
-					if (LevelFrameCount % 41 == 10)
-					{
-						matlist_000E924C[0].attr_texId = 84;
-						matlist_000E3610[0].attr_texId = 84;
-						matlist_000E8A20[0].attr_texId = 84;
-						matlist_000E43D0[0].attr_texId = 84;
-						matlist_000E7228[0].attr_texId = 84;
-						matlist_000EF078[0].attr_texId = 84;
-						matlist_000ECF80[0].attr_texId = 84;
-						matlist_000EABDC[0].attr_texId = 84;
-						matlist_000EBB3C[0].attr_texId = 84;
-					}
-					if (LevelFrameCount % 41 == 13)
-					{
-						matlist_000E924C[0].attr_texId = 85;
-						matlist_000E3610[0].attr_texId = 85;
-						matlist_000E8A20[0].attr_texId = 85;
-						matlist_000E43D0[0].attr_texId = 85;
-						matlist_000E7228[0].attr_texId = 85;
-						matlist_000EF078[0].attr_texId = 85;
-						matlist_000ECF80[0].attr_texId = 85;
-						matlist_000EABDC[0].attr_texId = 85;
-						matlist_000EBB3C[0].attr_texId = 85;
-					}
-					if (LevelFrameCount % 41 == 16)
-					{
-						matlist_000E924C[0].attr_texId = 86;
-						matlist_000E3610[0].attr_texId = 86;
-						matlist_000E8A20[0].attr_texId = 86;
-						matlist_000E43D0[0].attr_texId = 86;
-						matlist_000E7228[0].attr_texId = 86;
-						matlist_000EF078[0].attr_texId = 86;
-						matlist_000ECF80[0].attr_texId = 86;
-						matlist_000EABDC[0].attr_texId = 86;
-						matlist_000EBB3C[0].attr_texId = 86;
-					}
-					if (LevelFrameCount % 41 == 19)
-					{
-						matlist_000E924C[0].attr_texId = 87;
-						matlist_000E3610[0].attr_texId = 87;
-						matlist_000E8A20[0].attr_texId = 87;
-						matlist_000E43D0[0].attr_texId = 87;
-						matlist_000E7228[0].attr_texId = 87;
-						matlist_000EF078[0].attr_texId = 87;
-						matlist_000ECF80[0].attr_texId = 87;
-						matlist_000EABDC[0].attr_texId = 87;
-						matlist_000EBB3C[0].attr_texId = 87;
-					}
-					if (LevelFrameCount % 41 == 22)
-					{
-						matlist_000E924C[0].attr_texId = 88;
-						matlist_000E3610[0].attr_texId = 88;
-						matlist_000E8A20[0].attr_texId = 88;
-						matlist_000E43D0[0].attr_texId = 88;
-						matlist_000E7228[0].attr_texId = 88;
-						matlist_000EF078[0].attr_texId = 88;
-						matlist_000ECF80[0].attr_texId = 88;
-						matlist_000EABDC[0].attr_texId = 88;
-						matlist_000EBB3C[0].attr_texId = 88;
-					}
-					if (LevelFrameCount % 41 == 25)
-					{
-						matlist_000E924C[0].attr_texId = 89;
-						matlist_000E3610[0].attr_texId = 89;
-						matlist_000E8A20[0].attr_texId = 89;
-						matlist_000E43D0[0].attr_texId = 89;
-						matlist_000E7228[0].attr_texId = 89;
-						matlist_000EF078[0].attr_texId = 89;
-						matlist_000ECF80[0].attr_texId = 89;
-						matlist_000EABDC[0].attr_texId = 89;
-						matlist_000EBB3C[0].attr_texId = 89;
-					}
-					if (LevelFrameCount % 41 == 28)
-					{
-						matlist_000E924C[0].attr_texId = 90;
-						matlist_000E3610[0].attr_texId = 90;
-						matlist_000E8A20[0].attr_texId = 90;
-						matlist_000E43D0[0].attr_texId = 90;
-						matlist_000E7228[0].attr_texId = 90;
-						matlist_000EF078[0].attr_texId = 90;
-						matlist_000ECF80[0].attr_texId = 90;
-						matlist_000EABDC[0].attr_texId = 90;
-						matlist_000EBB3C[0].attr_texId = 90;
-					}
-					if (LevelFrameCount % 41 == 31)
-					{
-						matlist_000E924C[0].attr_texId = 91;
-						matlist_000E3610[0].attr_texId = 91;
-						matlist_000E8A20[0].attr_texId = 91;
-						matlist_000E43D0[0].attr_texId = 91;
-						matlist_000E7228[0].attr_texId = 91;
-						matlist_000EF078[0].attr_texId = 91;
-						matlist_000ECF80[0].attr_texId = 91;
-						matlist_000EABDC[0].attr_texId = 91;
-						matlist_000EBB3C[0].attr_texId = 91;
-					}
-					if (LevelFrameCount % 41 == 34)
-					{
-						matlist_000E924C[0].attr_texId = 92;
-						matlist_000E3610[0].attr_texId = 92;
-						matlist_000E8A20[0].attr_texId = 92;
-						matlist_000E43D0[0].attr_texId = 92;
-						matlist_000E7228[0].attr_texId = 92;
-						matlist_000EF078[0].attr_texId = 92;
-						matlist_000ECF80[0].attr_texId = 92;
-						matlist_000EABDC[0].attr_texId = 92;
-						matlist_000EBB3C[0].attr_texId = 92;
-					}
-					if (LevelFrameCount % 41 == 37)
-					{
-						matlist_000E924C[0].attr_texId = 93;
-						matlist_000E3610[0].attr_texId = 93;
-						matlist_000E8A20[0].attr_texId = 93;
-						matlist_000E43D0[0].attr_texId = 93;
-						matlist_000E7228[0].attr_texId = 93;
-						matlist_000EF078[0].attr_texId = 93;
-						matlist_000ECF80[0].attr_texId = 93;
-						matlist_000EABDC[0].attr_texId = 93;
-						matlist_000EBB3C[0].attr_texId = 93;
-					}
-					if (LevelFrameCount % 41 == 0)
-					{
-						matlist_000E924C[0].attr_texId = 94;
-						matlist_000E3610[0].attr_texId = 94;
-						matlist_000E8A20[0].attr_texId = 94;
-						matlist_000E43D0[0].attr_texId = 94;
-						matlist_000E7228[0].attr_texId = 94;
-						matlist_000EF078[0].attr_texId = 94;
-						matlist_000ECF80[0].attr_texId = 94;
-						matlist_000EABDC[0].attr_texId = 94;
-						matlist_000EBB3C[0].attr_texId = 94;
-					}
-				}
-				if (CurrentLevel == 7 && CurrentAct == 0 && GameState == 15)
-				{
-					if (LevelFrameCount % 41 == 1)
-					{
-						matlist_000584CC[0].attr_texId = 44;
-						matlist_00057E90[0].attr_texId = 44;
-						matlist_00059BE8[0].attr_texId = 44;
-					}
-					if (LevelFrameCount % 41 == 4)
-					{
-						matlist_000584CC[0].attr_texId = 45;
-						matlist_00057E90[0].attr_texId = 45;
-						matlist_00059BE8[0].attr_texId = 45;
-					}
-					if (LevelFrameCount % 41 == 7)
-					{
-						matlist_000584CC[0].attr_texId = 46;
-						matlist_00057E90[0].attr_texId = 46;
-						matlist_00059BE8[0].attr_texId = 46;
-					}
-					if (LevelFrameCount % 41 == 10)
-					{
-						matlist_000584CC[0].attr_texId = 47;
-						matlist_00057E90[0].attr_texId = 47;
-						matlist_00059BE8[0].attr_texId = 47;
-					}
-					if (LevelFrameCount % 41 == 13)
-					{
-						matlist_000584CC[0].attr_texId = 48;
-						matlist_00057E90[0].attr_texId = 48;
-						matlist_00059BE8[0].attr_texId = 48;
-					}
-					if (LevelFrameCount % 41 == 16)
-					{
-						matlist_000584CC[0].attr_texId = 49;
-						matlist_00057E90[0].attr_texId = 49;
-						matlist_00059BE8[0].attr_texId = 49;
-					}
-					if (LevelFrameCount % 41 == 19)
-					{
-						matlist_000584CC[0].attr_texId = 50;
-						matlist_00057E90[0].attr_texId = 50;
-						matlist_00059BE8[0].attr_texId = 50;
-					}
-					if (LevelFrameCount % 41 == 22)
-					{
-						matlist_000584CC[0].attr_texId = 51;
-						matlist_00057E90[0].attr_texId = 51;
-						matlist_00059BE8[0].attr_texId = 51;
-					}
-					if (LevelFrameCount % 41 == 25)
-					{
-						matlist_000584CC[0].attr_texId = 52;
-						matlist_00057E90[0].attr_texId = 52;
-						matlist_00059BE8[0].attr_texId = 52;
-					}
-					if (LevelFrameCount % 41 == 28)
-					{
-						matlist_000584CC[0].attr_texId = 53;
-						matlist_00057E90[0].attr_texId = 53;
-						matlist_00059BE8[0].attr_texId = 53;
-					}
-					if (LevelFrameCount % 41 == 31)
-					{
-						matlist_000584CC[0].attr_texId = 54;
-						matlist_00057E90[0].attr_texId = 54;
-						matlist_00059BE8[0].attr_texId = 54;
-					}
-					if (LevelFrameCount % 41 == 34)
-					{
-						matlist_000584CC[0].attr_texId = 55;
-						matlist_00057E90[0].attr_texId = 55;
-						matlist_00059BE8[0].attr_texId = 55;
-					}
-					if (LevelFrameCount % 41 == 37)
-					{
-						matlist_000584CC[0].attr_texId = 56;
-						matlist_00057E90[0].attr_texId = 56;
-						matlist_00059BE8[0].attr_texId = 56;
-					}
-					if (LevelFrameCount % 41 == 0)
-					{
-						matlist_000584CC[0].attr_texId = 57;
-						matlist_00057E90[0].attr_texId = 57;
-						matlist_00059BE8[0].attr_texId = 57;
-					}
-				}
-			}
+			if (animw1 > 57) animw1 = 44;
+			((NJS_OBJECT*)0x01FE9D7C)->basicdxmodel->mats[0].attr_texId = animw1;
+			matlist_000584CC[0].attr_texId = animw1;
+			matlist_00057E90[0].attr_texId = animw1;
+			matlist_00059BE8[0].attr_texId = animw1;
+			if (LevelFrameCount % 3 == 0) animw1++;
+		}
+		if (CurrentLevel == 7 && CurrentAct == 1 && GameState != 16)
+		{
+			if (animw2 > 94) animw2 = 81;
+			matlist_000E924C[0].attr_texId = animw2;
+			matlist_000E3610[0].attr_texId = animw2;
+			matlist_000E8A20[0].attr_texId = animw2;
+			matlist_000E43D0[0].attr_texId = animw2;
+			matlist_000E7228[0].attr_texId = animw2;
+			matlist_000EF078[0].attr_texId = animw2;
+			matlist_000ECF80[0].attr_texId = animw2;
+			matlist_000EABDC[0].attr_texId = animw2;
+			matlist_000EBB3C[0].attr_texId = animw2;
+			if (LevelFrameCount % 3 == 0) animw2++;
+		}	
+	}
 };
-}
-
