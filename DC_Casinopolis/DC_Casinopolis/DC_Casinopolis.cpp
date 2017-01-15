8fe3e4ca570d9f9f289888bf0d5a427fa7208267
PiKeyAr <pikeyar@sadx>
2017-01-15 11:05:04 +0900

    Optimized Casino water animations

--- DC_Casinopolis/DC_Casinopolis/DC_Casinopolis.cpp
+++ DC_Casinopolis/DC_Casinopolis/DC_Casinopolis.cpp
@@ -7,6 +7,8 @@
 #include "Casino4.h"
 
 static short CurrentPlayer = -1;
+static int anim1 = 67;
+static int anim2 = 7;
 
 PointerInfo pointers[] = {
 	ptrdecl(0x97DB28, &landtable_00025EAC),
@@ -95,149 +97,18 @@ extern "C"
 				for (int r2 = 0; r2 < LengthOfArray(uv_01A474F8); r2++)
 				uv_01A474F8[r2].v = uv_01A474F8_0[r2].v;
 			}
-			if (LevelFrameCount % 41 == 1)
-			{
-				matlist_00066F5C[1].attr_texId = 75;
-				matlist_01979784[0].attr_texId = 75;
-			}
-			if (LevelFrameCount % 41 == 4)
-			{
-				matlist_00066F5C[1].attr_texId = 68;
-				matlist_01979784[0].attr_texId = 68;
-			}
-			if (LevelFrameCount % 41 == 7)
-			{
-				matlist_00066F5C[1].attr_texId = 69;
-				matlist_01979784[0].attr_texId = 69;
-			}
-			if (LevelFrameCount % 41 == 10)
-			{
-				matlist_00066F5C[1].attr_texId = 70;
-				matlist_01979784[0].attr_texId = 70;
-			}
-			if (LevelFrameCount % 41 == 13)
-			{
-				matlist_00066F5C[1].attr_texId = 71;
-				matlist_01979784[0].attr_texId = 71;
-			}
-			if (LevelFrameCount % 41 == 16)
-			{
-				matlist_00066F5C[1].attr_texId = 72;
-				matlist_01979784[0].attr_texId = 72;
-			}
-			if (LevelFrameCount % 41 == 19)
-			{
-				matlist_00066F5C[1].attr_texId = 73;
-				matlist_01979784[0].attr_texId = 73;
-			}
-			if (LevelFrameCount % 41 == 22)
-			{
-				matlist_00066F5C[1].attr_texId = 74;
-				matlist_01979784[0].attr_texId = 74;
-			}
-			if (LevelFrameCount % 41 == 25)
-			{
-				matlist_00066F5C[1].attr_texId = 67;
-				matlist_01979784[0].attr_texId = 67;
-			}
-			if (LevelFrameCount % 41 == 28)
-			{
-				matlist_00066F5C[1].attr_texId = 76;
-				matlist_01979784[0].attr_texId = 76;
-			}
-			if (LevelFrameCount % 41 == 31)
-			{
-				matlist_00066F5C[1].attr_texId = 77;
-				matlist_01979784[0].attr_texId = 77;
-			}
-			if (LevelFrameCount % 41 == 34)
-			{
-				matlist_00066F5C[1].attr_texId = 78;
-				matlist_01979784[0].attr_texId = 78;
-			}
-			if (LevelFrameCount % 41 == 37)
-			{
-				matlist_00066F5C[1].attr_texId = 79;
-				matlist_01979784[0].attr_texId = 79;
-			}
-			if (LevelFrameCount % 41 == 0)
-			{
-				matlist_00066F5C[1].attr_texId = 80;
-				matlist_01979784[0].attr_texId = 80;
-			}
+			if (anim1 > 80) anim1 = 67;
+			matlist_00066F5C[1].attr_texId = anim1;
+			matlist_01979784[0].attr_texId = anim1;
+			if (LevelFrameCount % 3 == 0) anim1++;	
 		}
-		if (CurrentLevel == 9 && CurrentAct == 1)
+		if (CurrentLevel == 9 && CurrentAct == 1 && GameState != 16)
 		{
-			if (LevelFrameCount % 41 == 1)
-			{
-				matlist_000ACC44[0].attr_texId = 7;
-				matlist_000ACB40[0].attr_texId = 7;
-			}
-			if (LevelFrameCount % 41 == 4)
-			{
-				matlist_000ACC44[0].attr_texId = 10;
-				matlist_000ACB40[0].attr_texId = 10;
-			}
-			if (LevelFrameCount % 41 == 7)
-			{
-				matlist_000ACC44[0].attr_texId = 11;
-				matlist_000ACB40[0].attr_texId = 11;
-			}
-			if (LevelFrameCount % 41 == 10)
-			{
-				matlist_000ACC44[0].attr_texId = 12;
-				matlist_000ACB40[0].attr_texId = 12;
-			}
-			if (LevelFrameCount % 41 == 13)
-			{
-				matlist_000ACC44[0].attr_texId = 13;
-				matlist_000ACB40[0].attr_texId = 13;
-			}
-			if (LevelFrameCount % 41 == 16)
-			{
-				matlist_000ACC44[0].attr_texId = 14;
-				matlist_000ACB40[0].attr_texId = 14;
-			}
-			if (LevelFrameCount % 41 == 19)
-			{
-				matlist_000ACC44[0].attr_texId = 9;
-				matlist_000ACB40[0].attr_texId = 9;
-			}
-			if (LevelFrameCount % 41 == 22)
-			{
-				matlist_000ACC44[0].attr_texId = 15;
-				matlist_000ACB40[0].attr_texId = 15;
-			}
-			if (LevelFrameCount % 41 == 25)
-			{
-				matlist_000ACC44[0].attr_texId = 16;
-				matlist_000ACB40[0].attr_texId = 16;
-			}
-			if (LevelFrameCount % 41 == 28)
-			{
-				matlist_000ACC44[0].attr_texId = 17;
-				matlist_000ACB40[0].attr_texId = 17;
-			}
-			if (LevelFrameCount % 41 == 31)
-			{
-				matlist_000ACC44[0].attr_texId = 18;
-				matlist_000ACB40[0].attr_texId = 18;
-			}
-			if (LevelFrameCount % 41 == 34)
-			{
-				matlist_000ACC44[0].attr_texId = 19;
-				matlist_000ACB40[0].attr_texId = 19;
-			}
-			if (LevelFrameCount % 41 == 37)
-			{
-				matlist_000ACC44[0].attr_texId = 20;
-				matlist_000ACB40[0].attr_texId = 20;
-			}
-			if (LevelFrameCount % 41 == 0)
-			{
-				matlist_000ACC44[0].attr_texId = 21;
-				matlist_000ACB40[0].attr_texId = 21;
-			}
+			if (anim2 > 7 && anim2 < 9) anim2 = 9;
+			if (anim2 > 21) anim2 = 7;
+			matlist_000ACC44[0].attr_texId = anim2;
+			matlist_000ACB40[0].attr_texId = anim2;
+			if (LevelFrameCount % 3 == 0) anim2++;
 		}
 	}
 }
