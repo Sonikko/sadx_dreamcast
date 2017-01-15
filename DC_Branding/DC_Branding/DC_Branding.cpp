66da321acf9f5236a8919bb2274be35bad99619a
PiKeyAr <pikeyar@sadx>
2017-01-16 06:14:11 +0900

    Pause buttons DC style

--- DC_Branding/DC_Branding/DC_Branding.cpp
+++ DC_Branding/DC_Branding/DC_Branding.cpp
@@ -11,6 +11,13 @@ extern "C"
 		WriteData((void*)0x0050E5C2, 0x90, 3);
 		WriteData((void*)0x0050E5E2, 0x90, 3);
 		WriteData((void*)0x0042CCF3, 0x0F, 1);
+		WriteData((void*)0x00458125, 0x66, 3);
+		WriteData((void*)0x00458128, 0x3F, 1);
+		WriteData((void*)0x0045812A, 0x9A, 1);
+		WriteData((void*)0x0045812B, 0x99, 1);
+		WriteData((void*)0x0045812C, 0x19, 1);
+		WriteData((void*)0x0045812D, 0x3F, 1);
+		WriteData((void*)0x0045812F, 0, 4);
 	}
 }
 
