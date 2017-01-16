24c49cf16c1ea6a789534597554a5d8a29fe1643
PiKeyAr <pikeyar@sadx>
2017-01-16 09:30:26 +0900

    Better colors for button rectangle

--- DC_Branding/DC_Branding/DC_Branding.cpp
+++ DC_Branding/DC_Branding/DC_Branding.cpp
@@ -11,12 +11,14 @@ extern "C"
 		WriteData((void*)0x0050E5C2, 0x90, 3);
 		WriteData((void*)0x0050E5E2, 0x90, 3);
 		WriteData((void*)0x0042CCF3, 0x0F, 1);
-		WriteData((void*)0x00458125, 0x66, 3);
+		//Selection box B
+		WriteData((void*)0x00458125, 0x00, 2);
+		WriteData((void*)0x00458127, 0x80, 2);
 		WriteData((void*)0x00458128, 0x3F, 1);
-		WriteData((void*)0x0045812A, 0x9A, 1);
-		WriteData((void*)0x0045812B, 0x99, 1);
-		WriteData((void*)0x0045812C, 0x19, 1);
+		//Selection box G
+		WriteData((void*)0x0045812A, 0x33, 3);
 		WriteData((void*)0x0045812D, 0x3F, 1);
+		//Selection box R
 		WriteData((void*)0x0045812F, 0, 4);
 	}
 }
