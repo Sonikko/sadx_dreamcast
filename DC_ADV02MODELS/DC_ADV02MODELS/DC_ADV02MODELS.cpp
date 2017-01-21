f1c8fa7d2151703e0fccff1d7bc1295c77b60895
PiKeyAr <pikeyar@sadx>
2017-01-21 22:36:10 +0900

    Hopefully this fixed the UVs

--- DC_ADV02MODELS/DC_ADV02MODELS/DC_ADV02MODELS.cpp
+++ DC_ADV02MODELS/DC_ADV02MODELS/DC_ADV02MODELS.cpp
@@ -106,7 +106,7 @@ extern "C"  __declspec(dllexport) void __cdecl OnFrame()
 		{
 			uv_00075EC0[q].v = uv_00075EC0[q].v + 1;
 		}
-		if (uv_00075EC0[0].v >= 577)
+		if (uv_00075EC0[0].v > -322)
 		{
 			for (int r1 = 0; r1 < LengthOfArray(uv_00075EC0); r1++)
 			{
@@ -117,7 +117,7 @@ extern "C"  __declspec(dllexport) void __cdecl OnFrame()
 		{
 			uv_000755A4[q2].v = uv_000755A4[q2].v - 1;
 		}
-		if (uv_000755A4[0].v <= -502)
+		if (uv_000755A4[1].v < -251)
 		{
 			for (int r2 = 0; r2 < LengthOfArray(uv_000755A4); r2++)
 			{
@@ -172,10 +172,45 @@ extern "C"  __declspec(dllexport) void __cdecl OnFrame()
 			if (CurrentFogDist > -12000.0f) CurrentFogDist = CurrentFogDist - 32.0f;
 		}
 		for (int q6 = 0; q6 < LengthOfArray(uv_00162054); q6++) {uv_00162054[q6].v++; }
+		if (uv_00162054[0].v > 255)
+		{
+			for (int r3 = 0; r3 < LengthOfArray(uv_00162054); r3++)
+			{
+				uv_00162054[r3].v = uv_00162054_0[r3].v;
+			}
+		}
 		for (int q7 = 0; q7 < LengthOfArray(uv_001622D8); q7++) {uv_001622D8[q7].v++; }
+		if (uv_001622D8[0].v > 510)
+		{
+			for (int r4 = 0; r4 < LengthOfArray(uv_001622D8); r4++)
+			{
+				uv_001622D8[r4].v = uv_001622D8_0[r4].v;
+			}
+		}
 		for (int q3 = 0; q3 < LengthOfArray(uv_00160D9C); q3++)	{uv_00160D9C[q3].v--; }
+		if (uv_00160D9C[0].v < -4)
+		{
+			for (int r5 = 0; r5 < LengthOfArray(uv_00160D9C); r5++)
+			{
+				uv_00160D9C[r5].v = uv_00160D9C_0[r5].v;
+			}
+		}
 		for (int q4 = 0; q4 < LengthOfArray(uv_0016166C); q4++)	{uv_0016166C[q4].v++; }
+		if (uv_0016166C[0].v > 179)
+		{
+			for (int r6 = 0; r6 < LengthOfArray(uv_0016166C); r6++)
+			{
+				uv_0016166C[r6].v = uv_0016166C_0[r6].v;
+			}
+		}
 		for (int q5 = 0; q5 < LengthOfArray(uv_00161C18); q5++) {uv_00161C18[q5].v++; }
+		if (uv_00161C18[0].v > -473)
+		{
+			for (int r7 = 0; r7 < LengthOfArray(uv_00161C18); r7++)
+			{
+				uv_00161C18[r7].v = uv_00161C18_0[r7].v;
+			}
+		}
 	}
 }
 	
