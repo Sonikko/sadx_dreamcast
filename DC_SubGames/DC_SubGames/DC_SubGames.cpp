9eb9358a2b0e28760a914f8c749ca4e0692cd81c
PiKeyAr <pikeyar@sadx>
2017-01-23 18:38:33 +0900

    Sky Chase models

--- DC_SubGames/DC_SubGames/DC_SubGames.cpp
+++ DC_SubGames/DC_SubGames/DC_SubGames.cpp
@@ -1,7 +1,12 @@
 #include "stdafx.h"
+#include "SADXModLoader.h"
 #include "SandHill.h"
 #include "TwinkleCircuit.h"
-#include "SADXModLoader.h"
+#include "ECModel.h"
+//#include "Tornado1.h"
+//#include "Tornado2.h"
+//#include "Tornado2_Trans.h"
+//#include "Tornado2_Trans2.h"
 
 DataArray(FogData, FogData_SandHill, 0x0173BB74, 3);
 DataPointer(float, CurrentDrawDistance, 0x03ABDC74);
@@ -14,6 +19,11 @@ PointerInfo pointers[] = {
 extern "C" __declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
 extern "C" __declspec(dllexport) void cdecl Init()
 {
+	*(NJS_OBJECT *)0x02982F44 = object_0003FA40; //Egg Carrier model
+	//*(NJS_OBJECT *)0x028B1DA0 = object_00060C40; //Tornado model 1
+	//*(NJS_OBJECT *)0x027EB198 = object_000E95C8; //Tornado model 2
+	//*(NJS_OBJECT *)0x0280C158 = object_0010003C; //Tornado model 2 transformed
+	//*(NJS_OBJECT *)0x2863DEC = object_001506C8; //Tornado transformation
 	for (int i = 0; i < 3; i++)
 	{
 		FogData_SandHill[i].Color = 0xFFAAAA8C;
