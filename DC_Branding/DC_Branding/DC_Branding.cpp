929f1afb1f73b896445101b12b8ac2fd477dabe1
PiKeyAr <pikeyar@sadx>
2017-02-10 10:47:21 +0900

    A better way to stop titlescreen scrolling

--- DC_Branding/DC_Branding/DC_Branding.cpp
+++ DC_Branding/DC_Branding/DC_Branding.cpp
@@ -6,11 +6,9 @@ extern "C"
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
 	__declspec(dllexport) void __cdecl Init()
 	{
-		WriteData((void*)0x0050E842, 0x90, 3);
-		WriteData((void*)0x0050E864, 0x90, 3);
-		WriteData((void*)0x0050E5C2, 0x90, 3);
-		WriteData((void*)0x0050E5E2, 0x90, 3);
-		WriteData((void*)0x0042CCF3, 0x0F, 1);
+		WriteData((void*)0x0050E83B, 0x34, 1);
+		WriteData((void*)0x0050E83C, 0xF9, 1);
+		WriteData((void*)0x0042CCF3, 0x0F, 1); //Disable Sonic Team logo
 		//Selection box B
 		WriteData((void*)0x00458125, 0x00, 2);
 		WriteData((void*)0x00458127, 0x80, 2);
