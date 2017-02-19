4f3e4cbe526d1e497900f29894cb8488c15833f7
PiKeyAr <pikeyar@sadx>
2017-02-20 03:33:49 +0900

    Final Egg soundbank

--- SoundOverhaul/SoundOverhaul/SoundOverhaul.cpp
+++ SoundOverhaul/SoundOverhaul/SoundOverhaul.cpp
@@ -10,11 +10,20 @@ SoundFileInfo E101mkIISoundList_list[] = {
 	{ 5, "" }
 };
 
-
 SoundFileInfo soundlist97_list[] = {
 	{ 6, "E_00BF" }
 };
 
+
+SoundFileInfo FinalEggSoundList_list[] = {
+	{ 0, "COMMON_BANK00" },
+	{ 1, "FINAL_EGG_BANK01" },
+	{ 2, "CHECK_SHEET_BANK02" },
+	{ 4, "ENEMY_SET_BANK04" },
+	{ 5, "FINAL_EGG_BANK05" }
+};
+
+SoundList FinalEggSoundList = { arraylengthandptr(FinalEggSoundList_list) };
 SoundList E101mkIISoundList = { arraylengthandptr(E101mkIISoundList_list) };
 SoundList soundlist97 = { arraylengthandptr(soundlist97_list) };
 
@@ -25,6 +34,7 @@ extern "C"
 	{
 		*(SoundList*)0x90FDD0 = E101mkIISoundList;
 		*(SoundList*)0x90FFB8 = soundlist97;
-		WriteData((void*)0x0053881F, 0x90u, 2);
+		*(SoundList*)0x90FDE0 = FinalEggSoundList;
+		WriteData((void*)0x0053881F, 0x90u, 2); // Enable ambient sound in MR Final Egg base
 	}
 }
