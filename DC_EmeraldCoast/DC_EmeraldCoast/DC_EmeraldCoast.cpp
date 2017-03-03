bc56a6988a6e44291042c6cbda1d5ec071afbaeb
PiKeyAr <pikeyar@sadx>
2017-03-04 06:41:50 +0900

    Water textures restored and dynamic fog for Act 3

--- DC_EmeraldCoast/DC_EmeraldCoast/DC_EmeraldCoast.cpp
+++ DC_EmeraldCoast/DC_EmeraldCoast/DC_EmeraldCoast.cpp
@@ -11,10 +11,15 @@ static int anim3 = 42;
 static int anim4 = 71;
 static int anim5 = 50;
 static int anim6 = 65;
+static int anim7 = 57;
+static int anim8 = 80;
 static int animframe = 0;
-
+int water_anim = 17;
 DataPointer(int, FramerateSetting, 0x0389D7DC);
 DataPointer(NJS_VECTOR, CurrentSkybox, 0x03ABDC94);
+DataPointer(EntityData1*, Camera_Data1, 0x03B2CBB0);
+DataPointer(int, CurrentFogToggle, 0x03ABDC6C);
+DataPointer(float, CurrentFogLayer, 0x03ABDC60);
 
 PointerInfo pointers[] = {
 	ptrdecl(0x97DA28, &landtable_00081554),
@@ -26,15 +31,22 @@ extern "C" __declspec(dllexport) const PointerList Pointers = { arrayptrandlengt
 
 extern "C" __declspec(dllexport) void __cdecl Init()
 {
-	ResizeTextureList((NJS_TEXLIST*)0xF812AC, textures_ecoast1);
-	ResizeTextureList((NJS_TEXLIST*)0xEF553C, textures_ecoast2);
-	ResizeTextureList((NJS_TEXLIST*)0xE9A4CC, textures_ecoast3);
+	HMODULE SADXStyleWater = GetModuleHandle(L"SADXStyleWater");
+	DataArray(PVMEntry, BeachTexlists, 0x0102F408, 25);
 	DataArray(DrawDistance, DrawDist_EmeraldCoast1, 0x00E99D94, 3);
 	DataArray(DrawDistance, DrawDist_EmeraldCoast2, 0x00E99DAC, 3);
 	DataArray(DrawDistance, DrawDist_EmeraldCoast3, 0x00E99DC4, 3);
 	DataArray(FogData, EmeraldCoast1Fog, 0x00E99DDC, 3);
 	DataArray(FogData, EmeraldCoast2Fog, 0x00E99E0C, 3);
 	DataArray(FogData, EmeraldCoast3Fog, 0x00E99E3C, 3);
+	WriteData((void*)0x004F7749, 0x90, 5); //Kill second water in Act 2
+	WriteData((void*)0x004F77E9, 0x90, 5); //Kill second water in Act 3
+	if (SADXStyleWater == 0) WriteData((void*)0x004F8A58, 0, 1); //Kill that stupid Blending Mode thing
+	if (SADXStyleWater != 0) BeachTexlists[1].Name = "BEACH_SEAW";
+	ResizeTextureList((NJS_TEXLIST*)0x010C0508, 27); //BEACH_SEA texlist
+	ResizeTextureList((NJS_TEXLIST*)0xF812AC, textures_ecoast1);
+	ResizeTextureList((NJS_TEXLIST*)0xEF553C, textures_ecoast2);
+	ResizeTextureList((NJS_TEXLIST*)0xE9A4CC, textures_ecoast3);
 	for (int i = 0; i < 3; i++)
 	{
 		DrawDist_EmeraldCoast1[i].Maximum = -6000.0f;
@@ -43,6 +55,9 @@ extern "C" __declspec(dllexport) void __cdecl Init()
 		EmeraldCoast1Fog[i].Toggle = 0;
 		EmeraldCoast2Fog[i].Toggle = 0;
 		EmeraldCoast3Fog[i].Toggle = 0;
+		EmeraldCoast3Fog[i].Layer = -300.0f;
+		EmeraldCoast3Fog[i].Distance = -3000.0f;
+		EmeraldCoast3Fog[i].Color = 0xFFFFFFFF;
 	}
 }
 
@@ -56,7 +71,7 @@ extern "C" __declspec(dllexport) void __cdecl OnFrame()
 		((NJS_OBJECT *)0x103B37C)->evalflags &= ~NJD_EVAL_HIDE;
 	}
 	if (CurrentLevel == 1 && CurrentAct == 1)
-	{
+	{	
 		CurrentSkybox.x = 1.0f;
 		CurrentSkybox.y = 1.0f;
 		CurrentSkybox.z = 1.0f;
@@ -73,10 +88,13 @@ extern "C" __declspec(dllexport) void __cdecl OnFrame()
 	{
 		animframe++;
 	}
-	if (CurrentLevel == 1 && CurrentAct == 0 && GameState == 15)
+	if (CurrentLevel == 1 && CurrentAct == 0 && GameState != 16)
 		{
+			HMODULE SADXStyleWater = GetModuleHandle(L"SADXStyleWater");
+			if (water_anim > 26) water_anim = 17;
 			if (anim1 > 96) anim1 = 82;
 			if (anim2 > 81) anim2 = 67;
+			if (SADXStyleWater == 0) WriteData((void*)0x004F8B8F, water_anim, 1);
 			matlist_000755F8[0].attr_texId = anim1;
 			matlist_000759CC[0].attr_texId = anim1;
 			matlist_00075C74[0].attr_texId = anim1;
@@ -111,12 +129,18 @@ extern "C" __declspec(dllexport) void __cdecl OnFrame()
 			{
 				anim1++;
 				anim2++;
+				water_anim++;
 			}
 		}
-	if (CurrentLevel == 1 && CurrentAct == 1 && GameState == 15)
+	if (CurrentLevel == 1 && CurrentAct == 1 && GameState != 16)
 	{
+			HMODULE SADXStyleWater = GetModuleHandle(L"SADXStyleWater");
+			if (water_anim > 26) water_anim = 17;
 			if (anim3 > 56) anim3 = 42;
 			if (anim4 > 85) anim4 = 71;
+			if (anim7 > 70) anim7 = 57;
+			if (SADXStyleWater == 0) WriteData((void*)0x004F8B8F, water_anim, 1);
+			matlist_000D7BB8[0].attr_texId = anim7;
 			matlist_0008F1FC[0].attr_texId = anim3;
 			matlist_0008F7CC[2].attr_texId = anim3;
 			matlist_000907D4[4].attr_texId = anim3;
@@ -149,12 +173,29 @@ extern "C" __declspec(dllexport) void __cdecl OnFrame()
 			{
 				anim3++;
 				anim4++;
+				anim7++;
+				water_anim++;
 			}
 	}
 	if (CurrentLevel == 1 && CurrentAct == 2 && GameState != 16)
 	{
+		HMODULE SADXStyleWater = GetModuleHandle(L"SADXStyleWater");
+		if (Camera_Data1 != nullptr && Camera_Data1->Position.z > 1700)
+		{
+			CurrentFogToggle = 1;
+			if (CurrentFogLayer < -11) CurrentFogLayer = CurrentFogLayer + 10;
+		}
+		else
+		{
+			if (CurrentFogLayer > -300) CurrentFogLayer = CurrentFogLayer - 10;
+			if (CurrentFogLayer <= -300 ) CurrentFogToggle = 0;
+		}
 			if (anim5 > 64) anim5 = 50;
 			if (anim6 > 79) anim6 = 65;
+			if (anim8 > 93) anim8 = 80;
+			if (water_anim > 26) water_anim = 17;
+			if (SADXStyleWater == 0) WriteData((void*)0x004F8B8F, water_anim, 1);
+			matlist_00114D08[0].attr_texId = anim8;
 			matlist_00117310[0].attr_texId = anim5;
 			matlist_0011784C[0].attr_texId = anim5;
 			matlist_00117C5C[0].attr_texId = anim5;
@@ -202,6 +243,8 @@ extern "C" __declspec(dllexport) void __cdecl OnFrame()
 			{
 				anim5++;
 				anim6++;
+				anim8++;
+				water_anim++;
 			}
 	}
 }
