25407746e7979f23274ef769c318a64f47ce91e6
PiKeyAr <pikeyar@sadx>
2017-03-09 21:37:33 +0900

    16:9 title screen for DC Branding

--- DC_Branding/DC_Branding/DC_Branding.cpp
+++ DC_Branding/DC_Branding/DC_Branding.cpp
@@ -12,38 +12,67 @@ extern "C"
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
 	__declspec(dllexport) void __cdecl Init()
 	{
-		//*(char**)0x00892944 = "Sonic Adventure";
 		HMODULE DisableSA1Titlescreen = GetModuleHandle(L"DisableSA1Titlescreen");
 		if (DisableSA1Titlescreen == 0)
 		{
-		GUITexturePVMs[17].Name = "AVA_GTITLE0_ES";
-		GUITexturePVMs[18].Name = "AVA_TITLE_CMNS";
-		GUITexturePVMs[20].Name = "AVA_TITLE_BACK_ES";
-		GUITexturePVMs[29].Name = "AVA_TITLE_CMN_SMALLS";
+			if (float(HorizontalResolution) / float(VerticalResolution) < 1.5f) //Non-widescreen
+			{
+				GUITexturePVMs[17].Name = "AVA_GTITLE0_ES";
+				GUITexturePVMs[18].Name = "AVA_TITLE_CMNS";
+				GUITexturePVMs[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs[29].Name = "AVA_TITLE_CMN_SMALLS";
 
-		GUITexturePVMs2[17].Name = "AVA_GTITLE0_ES";
-		GUITexturePVMs2[18].Name = "AVA_TITLE_CMNS";
-		GUITexturePVMs2[20].Name = "AVA_TITLE_BACK_ES";
-		GUITexturePVMs2[29].Name = "AVA_TITLE_CMN_SMALLS";
+				GUITexturePVMs2[17].Name = "AVA_GTITLE0_ES";
+				GUITexturePVMs2[18].Name = "AVA_TITLE_CMNS";
+				GUITexturePVMs2[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs2[29].Name = "AVA_TITLE_CMN_SMALLS";
 
-		GUITexturePVMs3[17].Name = "AVA_GTITLE0_ES";
-		GUITexturePVMs3[18].Name = "AVA_TITLE_CMNS";
-		GUITexturePVMs3[20].Name = "AVA_TITLE_BACK_ES";
-		GUITexturePVMs3[29].Name = "AVA_TITLE_CMN_SMALLS";
+				GUITexturePVMs3[17].Name = "AVA_GTITLE0_ES";
+				GUITexturePVMs3[18].Name = "AVA_TITLE_CMNS";
+				GUITexturePVMs3[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs3[29].Name = "AVA_TITLE_CMN_SMALLS";
 
-		GUITexturePVMs4[17].Name = "AVA_GTITLE0_ES";
-		GUITexturePVMs4[18].Name = "AVA_TITLE_CMNS";
-		GUITexturePVMs4[20].Name = "AVA_TITLE_BACK_ES";
-		GUITexturePVMs4[29].Name = "AVA_TITLE_CMN_SMALLS";
+				GUITexturePVMs4[17].Name = "AVA_GTITLE0_ES";
+				GUITexturePVMs4[18].Name = "AVA_TITLE_CMNS";
+				GUITexturePVMs4[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs4[29].Name = "AVA_TITLE_CMN_SMALLS";
 
-		GUITexturePVMs5[17].Name = "AVA_GTITLE0_ES";
-		GUITexturePVMs5[18].Name = "AVA_TITLE_CMNS";
-		GUITexturePVMs5[20].Name = "AVA_TITLE_BACK_ES";
-		GUITexturePVMs5[29].Name = "AVA_TITLE_CMN_SMALLS";
-		WriteData((void*)0x0050E83B, 0x34, 1); //Disable titlescreen scrolling 1
-		WriteData((void*)0x0050E83C, 0xF9, 1); //Disable titlescreen scrolling 2
-		WriteData((void*)0x0050E5BB, 0x34, 1); //Disable titlescreen scrolling 1 (640x480)
-		WriteData((void*)0x0050E5BC, 0xF9, 1); //Disable titlescreen scrolling 2 (640x480)
+				GUITexturePVMs5[17].Name = "AVA_GTITLE0_ES";
+				GUITexturePVMs5[18].Name = "AVA_TITLE_CMNS";
+				GUITexturePVMs5[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs5[29].Name = "AVA_TITLE_CMN_SMALLS";
+			}
+			if (float(HorizontalResolution) / float(VerticalResolution) > 1.5f) //Widescreen
+			{
+				GUITexturePVMs[17].Name = "AVA_GTITLE0_E1";
+				GUITexturePVMs[18].Name = "AVA_TITLE_CMN1";
+				GUITexturePVMs[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs[29].Name = "AVA_TITLE_CMN_SMALLS";
+
+				GUITexturePVMs2[17].Name = "AVA_GTITLE0_E1";
+				GUITexturePVMs2[18].Name = "AVA_TITLE_CMN1";
+				GUITexturePVMs2[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs2[29].Name = "AVA_TITLE_CMN_SMALLS";
+
+				GUITexturePVMs3[17].Name = "AVA_GTITLE0_E1";
+				GUITexturePVMs3[18].Name = "AVA_TITLE_CMN1";
+				GUITexturePVMs3[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs3[29].Name = "AVA_TITLE_CMN_SMALLS";
+
+				GUITexturePVMs4[17].Name = "AVA_GTITLE0_E1";
+				GUITexturePVMs4[18].Name = "AVA_TITLE_CMN1";
+				GUITexturePVMs4[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs4[29].Name = "AVA_TITLE_CMN_SMALLS";
+
+				GUITexturePVMs5[17].Name = "AVA_GTITLE0_E1";
+				GUITexturePVMs5[18].Name = "AVA_TITLE_CMN1";
+				GUITexturePVMs5[20].Name = "AVA_TITLE_BACK_ES";
+				GUITexturePVMs5[29].Name = "AVA_TITLE_CMN_SMALLS";
+			}
+			WriteData((void*)0x0050E83B, 0x34, 1); //Disable titlescreen scrolling 1
+			WriteData((void*)0x0050E83C, 0xF9, 1); //Disable titlescreen scrolling 2
+			WriteData((void*)0x0050E5BB, 0x34, 1); //Disable titlescreen scrolling 1 (640x480)
+			WriteData((void*)0x0050E5BC, 0xF9, 1); //Disable titlescreen scrolling 2 (640x480)
 		}
 		WriteData((void*)0x0042CCF3, 0x0F, 1); //Disable Sonic Team logo
 		//Selection box B
