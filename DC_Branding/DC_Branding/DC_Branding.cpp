0def54db9b63b8128a7ec2e5385b8f021041bc94
PiKeyAr <pikeyar@sadx>
2017-03-26 17:22:58 +0900

    Customizable title screen

--- DC_Branding/DC_Branding/DC_Branding.cpp
+++ DC_Branding/DC_Branding/DC_Branding.cpp
@@ -6,14 +6,14 @@ DataArray(PVMEntry, GUITexturePVMs2, 0x007EEDE0, 30);
 DataArray(PVMEntry, GUITexturePVMs3, 0x007EEED0, 30);
 DataArray(PVMEntry, GUITexturePVMs4, 0x007EEFC0, 30);
 DataArray(PVMEntry, GUITexturePVMs5, 0x007EF0B0, 30);
-
+static float vstretchx = 0.3525f;
 extern "C"
 {
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
 	__declspec(dllexport) void __cdecl Init()
 	{
 		HMODULE DisableSA1Titlescreen = GetModuleHandle(L"DisableSA1Titlescreen");
-		if (DisableSA1Titlescreen == 0)
+		if (DisableSA1Titlescreen == 0 && float(HorizontalResolution) / float(VerticalResolution) < 2.2f)
 		{
 			if (float(HorizontalResolution) / float(VerticalResolution) < 1.5f) //Non-widescreen
 			{
@@ -74,7 +74,12 @@ extern "C"
 			WriteData((void*)0x0050E5BB, 0x34, 1); //Disable titlescreen scrolling 1 (640x480)
 			WriteData((void*)0x0050E5BC, 0xF9, 1); //Disable titlescreen scrolling 2 (640x480)
 			WriteData((float*)0x0050FFAA, 191.0f); //PressStart texture
-			WriteData((float*)0x0050FFB2, 365.0f); //PressStart texture
+			WriteData((float*)0x0050FFB2, 363.0f); //PressStart texture
+			WriteData((float*)0x0050E6E7, 0.0f); //Disable vertical offset
+			WriteData((char*)0x0050E6C2, 0x34, 1); //Disable horizontal offset
+			WriteData((char*)0x0050E6C3, 0xF9, 1); //Disable horizontal offset 
+			WriteData((char*)0x0050E6C4, 0x7E, 1); //Disable horizontal offset
+			WriteData((char*)0x0050E6C5, 0x00, 1); //Disable horizontal offset
 			//Title screen vertical stretching
 			WriteData((char*)0x0050E7C1, 0x98, 1);
 			WriteData((char*)0x0050E7C2, 0xCC, 1);
@@ -94,6 +99,21 @@ extern "C"
 			WriteData((char*)0x0050E6D3, 0xCC, 1);
 			WriteData((char*)0x0050E6D4, 0x7D, 1);
 			WriteData((char*)0x0050E6D5, 0x00, 1);
+			if (float(HorizontalResolution) / float(VerticalResolution) > 1.5f)
+			{
+				WriteData((char*)0x0050E6CC, 0xC0, 1);//Widescreen
+				WriteData((char*)0x0050E6CD, 0x28, 1);//Widescreen
+				WriteData((char*)0x0050E6CE, 0x89, 1);//Widescreen
+				WriteData((char*)0x0050E6CF, 0x00, 1);//Widescreen
+				WriteData((float*)0x0050FFAA, 207.0f); //PressStart texture X for widescreen
+				WriteData((float*)0x0050FFB2, 375.0f); //PressStart texture Y for widescreen
+				WriteData((float**)0x0050FFFF, &vstretchx); //Stretch the PressStart texture at 16:9
+				if (float(HorizontalResolution) / float(VerticalResolution) > 1.8f) WriteData((float*)0x0050FFB2, 390.0f); //PressStart texture for widescreen 4K
+				if (float(HorizontalResolution) / float(VerticalResolution) < 1.77f) WriteData((float*)0x0050FFB2, 360.0f); //PressStart texture Y for widescreen 1024x600
+				if (float(HorizontalResolution) / float(VerticalResolution) < 1.77f) WriteData((float*)0x0050FFAA, 185.0f); //PressStart texture X for widescreen 1024x600
+				if (float(HorizontalResolution) / float(VerticalResolution) == 1.6f) WriteData((float*)0x0050FFB2, 335.0f); //PressStart texture Y for widescreen 16:10
+				if (float(HorizontalResolution) / float(VerticalResolution) == 1.6f) WriteData((float*)0x0050FFAA, 215.0f); //PressStart texture X for widescreen 16:10
+			}
 		}
 		WriteData((void*)0x0042CCF3, 0x0F, 1); //Disable Sonic Team logo
 		//Selection box B
