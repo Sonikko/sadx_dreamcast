b13c439c8ce6b636461c12b3a5487cb41c0bb9f3
PiKeyAr <pikeyar@sadx>
2017-03-29 23:00:37 +0900

    Emerald shard lighting

--- Main_DCMods/DC_General/DC_General.cpp
+++ Main_DCMods/DC_General/DC_General.cpp
@@ -2,6 +2,8 @@
 #include <SADXModLoader.h>
 #include "Animals.h"
 #include "Itembox.h"
+#include "EmeShard.h"
+
 static PointerInfo jumps[] = {
 	// ItemBox
 	{ ItemBox_Display_Destroyed,	ItemBox_Display_Destroyed_Rotate },
@@ -24,6 +26,8 @@ extern "C"
 		((NJS_MODEL*)0x008BE620)->mats[0].attrflags |= NJD_FLAG_IGNORE_SPECULAR;
 		((NJS_MODEL*)0x008BE620)->mats[1].attrflags |= NJD_FLAG_IGNORE_SPECULAR; //end
 		WriteData((char*)0x004D7712, 2, 1); //Animal bubble blending mode
+		WriteData((char*)0x004A2DBB, 1, 1); //Emerald piece blending mode
+		*(NJS_OBJECT*)0x008BA6D4 = object_0019BC48; //Emerald piece
 		//ResizeTextureList((NJS_TEXLIST*)0x0092ACE4, 5); //GOMA texlist
 		//*(NJS_OBJECT*)0x92A2D0 = object_00157760; //GOMA
 		//*(NJS_ACTION*)0x949FCC = action__158830; //GOMA animation
