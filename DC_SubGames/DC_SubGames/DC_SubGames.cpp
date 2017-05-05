7d41c319b08262f92b593d6cc6fe152bea2fb9ae
PiKeyAr <pikeyar@sadx>
2017-05-06 02:35:34 +0900

    Various Sky Chase models

--- DC_SubGames/DC_SubGames/DC_SubGames.cpp
+++ DC_SubGames/DC_SubGames/DC_SubGames.cpp
@@ -2,11 +2,7 @@
 #include <SADXModLoader.h>
 #include "SandHill.h"
 #include "TwinkleCircuit.h"
-#include "ECModel.h"
-//#include "Tornado1.h"
-//#include "Tornado2.h"
-//#include "Tornado2_Trans.h"
-//#include "Tornado2_Trans2.h"
+#include "SkyChaseModels.h"
 
 DataArray(FogData, FogData_SandHill, 0x0173BB74, 3);
 DataPointer(float, CurrentDrawDistance, 0x03ABDC74);
@@ -20,10 +16,13 @@ extern "C" __declspec(dllexport) const PointerList Pointers = { arrayptrandlengt
 extern "C" __declspec(dllexport) void cdecl Init()
 {
 	*(NJS_OBJECT *)0x02982F44 = object_0003FA40; //Egg Carrier model
-	//*(NJS_OBJECT *)0x028B1DA0 = object_00060C40; //Tornado model 1
-	//*(NJS_OBJECT *)0x027EB198 = object_000E95C8; //Tornado model 2
-	//*(NJS_OBJECT *)0x0280C158 = object_0010003C; //Tornado model 2 transformed
-	//*(NJS_OBJECT *)0x2863DEC = object_001506C8; //Tornado transformation
+	*(NJS_OBJECT *)0x298A894 = object_00047110; //Hodai
+	*(NJS_OBJECT *)0x2941B2C = object_0001342C; //Kirai
+	*(NJS_OBJECT *)0x017424DC = object_0006EA40; //Sand Hill ramp
+	((NJS_ACTION*)0x28E596C)->object = &object_0009153C; //Beam in Act 1
+	((NJS_ACTION*)0x2996C74)->object = &object_0004AEE0; //Beam in Act 2
+	*(NJS_OBJECT *)0x028E2C88 = object_0009153C; //Beam in Act 1
+	*(NJS_OBJECT *)0x0298E7D0 = object_0004AEE0; //Beam in Act 2
 	for (int i = 0; i < 3; i++)
 	{
 		FogData_SandHill[i].Color = 0xFFAAAA8C;
