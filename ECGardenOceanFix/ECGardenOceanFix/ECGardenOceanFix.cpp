e32e9215079763bd7cd305828822470189961874
PiKeyAr <pikeyar@sadx>
2017-06-01 05:31:23 +0900

    Why is this even included here

--- ECGardenOceanFix/ECGardenOceanFix/ECGardenOceanFix.cpp
+++ ECGardenOceanFix/ECGardenOceanFix/ECGardenOceanFix.cpp
@@ -6,6 +6,6 @@ extern "C"
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
 	__declspec(dllexport) void __cdecl Init()
 	{
-		((LandTable *)0x3405E54)->COLList[74].Flags = 0x84000002; //EC garden ocean animation
+		((LandTable *)0x3405E54)->Col[74].Flags = 0x84000002; //EC garden ocean animation
 	}
 }
