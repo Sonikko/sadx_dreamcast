b765b3997d4641d5d5dced27d71ca5681443a5cf
PiKeyAr <pikeyar@sadx>
2017-06-07 06:35:09 +0900

    Remove OBJ_REGULAR replacements

--- HD_GUI/HD_GUI.cpp
+++ HD_GUI/HD_GUI.cpp
@@ -10,11 +10,11 @@ extern "C"
 		HMODULE DC_Branding = GetModuleHandle(L"DC_Branding");
 		HMODULE DC_SubGames = GetModuleHandle(L"DC_SubGames");
 		if (DC_Branding == 0) WriteData((char*)0x007DDB2C, 0x44, 1); //Load CON_REGULAR_D with DX texture
-		*(NJS_MODEL_SADX*)0x008B6010 = attach_00197698; //Normal spring
-		*(NJS_MODEL_SADX*)0x008B5498 = attach_00196B5C; //Spring B
-		*(NJS_MODEL_SADX*)0x008C6624 = attach_001A7820; //Spring H
-		*(NJS_MODEL_SADX*)0x008BFEC8 = attach_001A127C; //Rocket platform
-		*(NJS_MODEL_SADX*)0x008BE168 = attach_0019F5CC; //Balloon
+		//*(NJS_MODEL_SADX*)0x008B6010 = attach_00197698; //Normal spring
+		//*(NJS_MODEL_SADX*)0x008B5498 = attach_00196B5C; //Spring B
+		//*(NJS_MODEL_SADX*)0x008C6624 = attach_001A7820; //Spring H
+		//*(NJS_MODEL_SADX*)0x008BFEC8 = attach_001A127C; //Rocket platform
+		//*(NJS_MODEL_SADX*)0x008BE168 = attach_0019F5CC; //Balloon
 		if (DC_SubGames == 0)
 		{
 		*(NJS_OBJECT *)0x02982F44 = object_0003FA40; //Egg Carrier model
