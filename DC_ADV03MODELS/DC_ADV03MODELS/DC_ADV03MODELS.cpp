ce6cf6923954b4b0f8a5cab360691c114b5b85a8
PiKeyAr <pikeyar@sadx>
2017-08-10 03:24:19 +0900

    Lighting updates for various models

--- DC_ADV03MODELS/DC_ADV03MODELS/DC_ADV03MODELS.cpp
+++ DC_ADV03MODELS/DC_ADV03MODELS/DC_ADV03MODELS.cpp
@@ -5,6 +5,7 @@
 #include "ADV03_00.h"
 #include "ADV03_01.h"
 #include "ADV03_02.h"
+#include "Past_objects.h"
 #include <lanternapi.h>
 
 HMODULE Past = GetModuleHandle(L"ADV03MODELS");
@@ -21,56 +22,16 @@ NJS_MATERIAL* SecondCharacterSpecular[] = {
 	(NJS_MATERIAL*)((size_t)Past + 0x0013CA54),
 };
 
+NJS_MATERIAL* ObjectSpecular[] = {
+//Tree
+	(NJS_MATERIAL*)((size_t)Past + 0x0011F030),
+	(NJS_MATERIAL*)((size_t)Past + 0x0011F044),
+	(NJS_MATERIAL*)((size_t)Past + 0x0011E100),
+	(NJS_MATERIAL*)((size_t)Past + 0x0011E114),
+};
+
 NJS_MATERIAL* FirstCharacterSpecular[] = {
-	//Tikal
-	(NJS_MATERIAL*)((size_t)Past + 0x001602E0),
-	(NJS_MATERIAL*)((size_t)Past + 0x001602F4),
-	(NJS_MATERIAL*)((size_t)Past + 0x00160308),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016031C),
-	(NJS_MATERIAL*)((size_t)Past + 0x001604B8),
-	(NJS_MATERIAL*)((size_t)Past + 0x001604CC),
-	(NJS_MATERIAL*)((size_t)Past + 0x00160668),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016088C),
-	(NJS_MATERIAL*)((size_t)Past + 0x00160BD4),
-	(NJS_MATERIAL*)((size_t)Past + 0x00160E2C),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016108C),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016126C),
-	(NJS_MATERIAL*)((size_t)Past + 0x001614C4),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016180C),
-	(NJS_MATERIAL*)((size_t)Past + 0x00161A54),
-	(NJS_MATERIAL*)((size_t)Past + 0x00161CC4),
-	(NJS_MATERIAL*)((size_t)Past + 0x00161EA4),
-	(NJS_MATERIAL*)((size_t)Past + 0x001623FC),
-	(NJS_MATERIAL*)((size_t)Past + 0x00162410),
-	(NJS_MATERIAL*)((size_t)Past + 0x00162424),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016268C),
-	(NJS_MATERIAL*)((size_t)Past + 0x001628F4),
-	(NJS_MATERIAL*)((size_t)Past + 0x00164858),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016486C),
-	(NJS_MATERIAL*)((size_t)Past + 0x00164880),
-	(NJS_MATERIAL*)((size_t)Past + 0x00164894),
-	(NJS_MATERIAL*)((size_t)Past + 0x001648A8),
-	(NJS_MATERIAL*)((size_t)Past + 0x00164FD4),
-	(NJS_MATERIAL*)((size_t)Past + 0x001651DC),
-	(NJS_MATERIAL*)((size_t)Past + 0x001653E4),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016568C),
-	(NJS_MATERIAL*)((size_t)Past + 0x00166268),
-	(NJS_MATERIAL*)((size_t)Past + 0x00166548),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016680C),
-	(NJS_MATERIAL*)((size_t)Past + 0x00166C58),
-	(NJS_MATERIAL*)((size_t)Past + 0x00166C6C),
-	(NJS_MATERIAL*)((size_t)Past + 0x00166EB4),
-	(NJS_MATERIAL*)((size_t)Past + 0x00166EC8),
-	(NJS_MATERIAL*)((size_t)Past + 0x00167104),
-	(NJS_MATERIAL*)((size_t)Past + 0x00167C28),
-	(NJS_MATERIAL*)((size_t)Past + 0x00167F08),
-	(NJS_MATERIAL*)((size_t)Past + 0x001681BC),
-	(NJS_MATERIAL*)((size_t)Past + 0x001685F8),
-	(NJS_MATERIAL*)((size_t)Past + 0x0016860C),
-	(NJS_MATERIAL*)((size_t)Past + 0x00168854),
-	(NJS_MATERIAL*)((size_t)Past + 0x00168868),
-	(NJS_MATERIAL*)((size_t)Past + 0x00168A94),
-	(NJS_MATERIAL*)((size_t)Past + 0x00168C8C),
+	//Tikal (NPC model I guess?)
 	//Chao
 	(NJS_MATERIAL*)((size_t)Past + 0x0013C668),
 	(NJS_MATERIAL*)((size_t)Past + 0x0013C190),
@@ -84,7 +45,6 @@ NJS_MATERIAL* FirstCharacterSpecular[] = {
 	(NJS_MATERIAL*)((size_t)Past + 0x0013A94C),
 	(NJS_MATERIAL*)((size_t)Past + 0x0013A4DC),
 	(NJS_MATERIAL*)((size_t)Past + 0x0013A068),
-	(NJS_MATERIAL*)((size_t)Past + 0x0013CA54),
 	(NJS_MATERIAL*)((size_t)Past + 0x0013C668),
 	(NJS_MATERIAL*)((size_t)Past + 0x0013C190),
 	(NJS_MATERIAL*)((size_t)Past + 0x0013BCB8),
@@ -115,6 +75,14 @@ bool ForceFirstCharacterSpecular(NJS_MATERIAL* material, Uint32 flags)
 	return true;
 }
 
+bool ForceObjectSpecular(NJS_MATERIAL* material, Uint32 flags)
+{
+	set_diffuse(0, false);
+	set_specular(1, false);
+	use_default_diffuse(true);
+	return true;
+}
+
 extern "C" __declspec(dllexport) void __cdecl Init(const char *path, const HelperFunctions &helperFunctions)
 {
 	HMODULE handle = GetModuleHandle(L"ADV03MODELS");
@@ -122,8 +90,9 @@ extern "C" __declspec(dllexport) void __cdecl Init(const char *path, const Helpe
 	HMODULE Lantern = GetModuleHandle(L"sadx-dc-lighting");
 	if (handle != nullptr && Lantern != nullptr && GetProcAddress(Lantern, "material_register") != nullptr)
 	{
-		material_register(FirstCharacterSpecular, LengthOfArray(FirstCharacterSpecular), &ForceFirstCharacterSpecular);
 		material_register(SecondCharacterSpecular, LengthOfArray(SecondCharacterSpecular), &ForceSecondCharacterSpecular);
+		material_register(FirstCharacterSpecular, LengthOfArray(FirstCharacterSpecular), &ForceFirstCharacterSpecular);
+		material_register(ObjectSpecular, LengthOfArray(ObjectSpecular), &ForceObjectSpecular);
 	}
 	if (SADXStyleWater != 0)
 	{
@@ -186,6 +155,7 @@ extern "C" __declspec(dllexport) void __cdecl Init(const char *path, const Helpe
 	___ADV03_OBJECTS[16] = &object_0001EDDC; //tree 16
 	___ADV03_OBJECTS[17] = &object_0001EDDC; //tree 17
 	___ADV03_OBJECTS[15] = &object_00027158; //small tree shadow
+	___ADV03_OBJECTS[13] = &object_00016CA0; //OWell
 	___ADV03_OBJECTS[18] = &object_00027054; //well shadow
 }
 
