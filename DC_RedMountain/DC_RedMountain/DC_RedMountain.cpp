1409481cd139d7a47578f7331daa1734fac0e18d
PiKeyAr <pikeyar@sadx>
2017-12-09 02:26:33 +0900

    Better UVs, depth sorting for clouds, better material handling

--- DC_RedMountain/DC_RedMountain/DC_RedMountain.cpp
+++ DC_RedMountain/DC_RedMountain/DC_RedMountain.cpp
@@ -13,11 +13,17 @@ PointerInfo pointers[] = {
 	ptrdecl(0x97DAB0, &landtable_0001E358)
 };
 
+DataPointer(int, FramerateSetting, 0x0389D7DC);
+DataPointer(float, DrawQueueDepthBias, 0x03ABD9C0);
 DataPointer(int, DroppedFrames, 0x03B1117C);
 DataPointer(float, CurrentDrawDistance, 0x03ABDC74);
 DataPointer(signed int, ClipLevel, 0x03ABDCF0);
 DataPointer(NJS_TEXLIST, OBJ_MOUNTAIN_TEXLIST, 0x024208A8);
 FunctionPointer(double, sub_789320, (float), 0x789320);
+FunctionPointer(void, sub_600BF0, (ObjectMaster *a1, NJS_OBJECT *a2), 0x600BF0);
+
+static int UVShift1 = 0;
+static int UVShift2 = 0;
 
 NJS_MATERIAL* LevelSpecular[] = {
 	//OSaku2
@@ -74,116 +80,31 @@ bool ForceWhiteDiffuse(NJS_MATERIAL* material, Uint32 flags)
 	return true;
 }
 
-void __cdecl sub_600BF0(ObjectMaster *a1, NJS_OBJECT *a2)
+void RenderRMSky1(ObjectMaster *a1, NJS_OBJECT *a2)
 {
-	EntityData1 *v2; // edi@1
-	long double v3; // st7@3
-	double v4; // st7@7
-	double v5; // st7@7
-	float v6; // ST08_4@11
-	float v7; // ST08_4@11
-	signed int v8; // esi@12
-	float z; // ST24_4@13
-	float XDist; // ST00_4@13
-	unsigned __int8 v11; // sf@14
-	unsigned __int8 v12; // of@14
-	signed int v13; // [sp+1Ch] [bp-20h]@11
-	signed int v14; // [sp+20h] [bp-1Ch]@12
-	float v15; // [sp+28h] [bp-14h]@11
-	D3DCOLORVALUE v16; // [sp+2Ch] [bp-10h]@3
-	float v17; // [sp+40h] [bp+4h]@7
-	float v18; // [sp+40h] [bp+4h]@11
-
-	v2 = a1->Data1;
-	if (!DroppedFrames && ClipLevel <= 0)
-	{
-		v16.r = Camera_Data1->Position.x;
-		v16.g = Camera_Data1->Position.y;
-		v16.b = Camera_Data1->Position.z;
-		v3 = 1.0f - fabs(v16.g - v2->Position.y) * 0.025f;
-		v2->Scale.y = v3;
-		if (v3 < 0)
-		{
-			v2->Scale.y = 0;
-		}
-		if (v2->Scale.y > 0.8f)
-		{
-			v2->Scale.y = 0.8f;
-		}
-		njSetTexture(&OBJ_MOUNTAIN_TEXLIST);
-		v4 = v2->Scale.y * -0.8f - 0.2f;
-		v16.a = v4;
-		v16.b = v4;
-		v16.g = v4;
-		v16.r = v4;
-		SetMaterialAndSpriteColor((NJS_ARGB *)&v16);
-		v5 = Camera_Data1->Position.x;
-		v17 = Camera_Data1->Position.z;
-		if (v5 < 0)
-		{
-			v5 = v5 - 2000.0f;
-		}
-		if (v17 < 0)
-		{
-			v17 = v17 - 2000.0f;
-		}
-		v6 = (v5 + 1000.0f) * 0.0005f;
-		v15 = sub_789320(v6) * 2000.0f;
-		v7 = (v17 + 1000.0f) * 0.0005f;
-		v13 = -1;
-		v18 = sub_789320(v7) * 2000.0f;
-		do
-		{
-			v8 = -1;
-			v14 = -1;
-			do
-			{
-				njPushMatrix(0);
-				XDist = (double)v14 * 2000.0f + v15;
-				z = (double)v13 * 2000.0f + v18;
-				njTranslate(0, XDist, v2->Position.y, z);
-				ProcessModelNode_A_Wrapper((NJS_OBJECT *)a2, QueuedModelFlagsB_SomeTextureThing, 1.0f);
-				njPopMatrix(1u);
-				v14 = ++v8;
-			} while (v8 < 2);
-			v12 = (v13 + 1) - 2;
-			v11 = v13++ - 1 < 0;
-		} while (v11 | v12);
-		ClampGlobalColorThing_Thing();
-	}
+	DrawQueueDepthBias = 8000.0f;
+	sub_600BF0(a1, &object_0206CAA8);
 }
 
-void __cdecl sub_600BF0Z(ObjectMaster *a1, NJS_OBJECT *a2)
+void RenderRMSky2(ObjectMaster *a1, NJS_OBJECT *a2)
 {
-	EntityData1 *v2; // edi@1
-	long double v3; // st7@3
-	double v4; // st7@7
-	double v5; // st7@7
-	float v6; // ST08_4@11
-	float v7; // ST08_4@11
-	signed int v8; // esi@12
-	float z; // ST24_4@13
-	float XDist; // ST00_4@13
-	unsigned __int8 v11; // sf@14
-	unsigned __int8 v12; // of@14
-	signed int v13; // [sp+1Ch] [bp-20h]@11
-	signed int v14; // [sp+20h] [bp-1Ch]@12
-	float v15; // [sp+28h] [bp-14h]@11
-	D3DCOLORVALUE v16; // [sp+2Ch] [bp-10h]@3
-	float v17; // [sp+40h] [bp+4h]@7
-	float v18; // [sp+40h] [bp+4h]@11
-	njSetTexture(&OBJ_MOUNTAIN_TEXLIST);
-	njPushMatrix(0);
-	ProcessModelNode_A_Wrapper((NJS_OBJECT *)a2, QueuedModelFlagsB_SomeTextureThing, 1.0);
-	njPopMatrix(1u);
+	DrawQueueDepthBias = 6000.0f;
+	sub_600BF0(a1, &object_0206CAA8_2);
 }
 
+void SetCloudColor(NJS_ARGB *a)
+{
+	SetMaterialAndSpriteColor_Float(0.2f+a->a, 0.2f + a->r, 0.2f + a->g, 0.2f + a->b);
+}
 extern "C"
 {
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
 	__declspec(dllexport) const PointerList Pointers = { arrayptrandlength(pointers) };
 	__declspec(dllexport) void __cdecl Init(const char *path)
 	{
+		WriteCall((void*)0x00600CA3, SetCloudColor);
+		WriteCall((void*)0x006011D8, RenderRMSky1);
+		WriteCall((void*)0x0060121C, RenderRMSky2);
 		HMODULE Lantern = GetModuleHandle(L"sadx-dc-lighting");
 		if (Lantern != nullptr && GetProcAddress(Lantern, "material_register") != nullptr)
 		{
@@ -191,7 +112,6 @@ extern "C"
 			material_register(ObjectSpecular, LengthOfArray(ObjectSpecular), &ForceObjectSpecular);
 			material_register(WhiteDiffuse, LengthOfArray(WhiteDiffuse), &ForceWhiteDiffuse);
 		}
-		//WriteJump((void*)0x600BF0, sub_600BF0);
 		*(NJS_OBJECT*)0x248213C = object_0208213C; //Lamp1
 		*(NJS_OBJECT*)0x2482D94 = object_02082D94; //Lamp2
 		//Standing light specular
@@ -203,18 +123,13 @@ extern "C"
 		*(NJS_OBJECT*)0x246AB24 = object_001821C0; //O Bpole 2
 		*(NJS_MODEL_SADX*)0x2466818 = attach_0017DF2C; //Bridge piece
 		*(NJS_MODEL_SADX*)0x2466568 = attach_0017DC94; //Bridge piece 2
-		*(NJS_OBJECT*)0x246CAA8 = object_0206CAA8_2; //Cloud
 		WriteData((float*)0x006011ED, 30.0f); //Cloud height (Knuckles)
 		WriteData((float*)0x006011A9, 60.0f); //Cloud height (Knuckles)
 		*(NJS_MODEL_SADX*)0x24390BC = attach_00151A8C; //Light thing
 		*(NJS_MODEL_SADX*)0x24394CC = attach_001515D8; //Light thing propeller 1
 		*(NJS_MODEL_SADX*)0x24392C4 = attach_001513C8; //Light thing propeller 2
-		*(NJS_OBJECT*)0x2439964 = object_001511FC;
+		*(NJS_OBJECT*)0x2439964 = object_001511FC; //Standing light thing (object)
 		WriteData((void*)0x0060C981, 3, 1); //Light thing blending mode
-		matlist_0206C9D4_2[0].diffuse.color = 0xEFFFFFFF;
-		matlist_0206C9D4[0].diffuse.color = 0xEFFFFFFF;
-		WriteData((void*)0x00600C8F, 0x58, sizeof(char));
-		WriteData((NJS_OBJECT**)0x006011CC, (NJS_OBJECT*)&object_0206CAA8);
 		ResizeTextureList((NJS_TEXLIST*)0x230FDF4, textures_mountain1);
 		ResizeTextureList((NJS_TEXLIST*)0x229B8CC, textures_mountain2);
 		ResizeTextureList((NJS_TEXLIST*)0x224096C, textures_mountain3);
@@ -250,16 +165,12 @@ extern "C"
 	{
 		if (CurrentLevel == 5 && GameState != 16)
 		{
+			UVShift1 = (UVShift1 - 1 * FramerateSetting) % 255;
+			UVShift2 = (UVShift2 - 2 * FramerateSetting) % 255;
 			for (int q = 0; q < LengthOfArray(uv_0206C9F0); q++)
 			{
-				uv_0206C9F0[q].u = uv_0206C9F0[q].u - 1;
-			}
-			if (uv_0206C9F0[3].u < -255)
-			{
-				uv_0206C9F0[0].u = 0;
-				uv_0206C9F0[1].u = 0;
-				uv_0206C9F0[2].u = 765;
-				uv_0206C9F0[3].u = 765;
+				uv_0206C9F0[q].u = uv_0206C9F0_0[q].u + UVShift1;
+				uv_0206C9F0_2[q].u = uv_0206C9F0_0[q].u + UVShift2;
 			}
 			if (CurrentAct == 1 && Camera_Data1 != nullptr)
 			{
