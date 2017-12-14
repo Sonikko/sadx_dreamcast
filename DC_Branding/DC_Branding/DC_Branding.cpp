c83301910f1d12c4e3cccb463ac070606a541a10
PiKeyAr <pikeyar@sadx>
2017-12-14 17:48:43 +0900

    Update mod loader, 640x480 titlescreen ripples

--- DC_Branding/DC_Branding/DC_Branding.cpp
+++ DC_Branding/DC_Branding/DC_Branding.cpp
@@ -1,9 +1,6 @@
-#include "stdafx.h"
 #include <SADXModLoader.h>
 #include <IniFile.hpp>
 
-DataPointer(NJS_TEXLIST, ava_title_e_TEXLIST, 0x010D7C78);
-DataPointer(NJS_TEXLIST, ava_title_back_e_TEXLIST, 0x010D7C80);
 DataPointer(int, DroppedFrames, 0x03B1117C);
 DataArray(PVMEntry, GUITexturePVMs, 0x007EECF0, 30);
 DataArray(PVMEntry, GUITexturePVMs2, 0x007EEDE0, 30);
@@ -196,6 +193,58 @@ void DrawTitleBG()
 	}
 }
 
+void DrawTitleBG_640()
+{
+	int tn;
+	float xpos;
+	float ypos;
+	float scaleX;
+	float scaleY;
+	if (!DroppedFrames)
+	{
+		if (titledrawn != titleframe)
+		{
+			if (titleframe > 22) titleframe = 0;
+			if (RipplesOn == false) tn = 0; else tn = 8 + (titleframe * 8);
+			scaleX = 1.0f;
+			scaleY = 1.0f;
+
+			xpos = 0;
+			ypos = 0;
+			DrawBG(tn, xpos, ypos, 2.0f, scaleX, scaleY);
+
+			xpos = 256;
+			ypos = 0;
+			DrawBG(tn + 1, xpos, ypos, 2.0f, scaleX, scaleY);
+
+			xpos = 0;
+			ypos = 256;
+			DrawBG(tn + 2, xpos, ypos, 2.0f, scaleX, scaleY);
+
+			xpos = 256;
+			ypos = 256;
+			DrawBG(tn + 3, xpos, ypos, 2.0f, scaleX, scaleY);
+
+			xpos = 512;
+			ypos = 0;
+			DrawBG(tn + 4, xpos, ypos, 2.0f, scaleX, scaleY);
+
+			xpos = 512;
+			ypos = 128;
+			DrawBG(tn + 5, xpos, ypos, 2.0f, scaleX, scaleY);
+
+			xpos = 512;
+			ypos = 256;
+			DrawBG(tn + 6, xpos, ypos, 2.0f, scaleX, scaleY);
+
+			xpos = 512;
+			ypos = 384;
+			DrawBG(tn + 7, xpos, ypos, 2.0f, scaleX, scaleY);
+			titledrawn = titleframe;
+		}
+	}
+}
+
 void DrawLogo()
 {
 	float xpos;
@@ -252,6 +301,44 @@ void DrawLogo()
 	}
 }
 
+void DrawLogo_640()
+{
+	float xpos;
+	float ypos;
+	if (RipplesOn == true && logodrawn != logoframe)
+	{
+		njTextureShadingMode(1);
+		njSetTexture((NJS_TEXLIST*)0x010D7C48); //AVA_GTITLE0_E
+		if (logoframe > 128) logoframe = 0;
+		//Draw logo
+		SetVtxColorB(0xFFFFFFFF);
+		DrawBG(4, 64, 81, 1.2f, 1.0f, 1.0f);
+		//Draw logo overlay
+		if (DrawOverlay == true) DrawBG(5, 64, 81, 1.2f, 1.0f, 1.0f);
+		//Draw Sonic Team logo
+		DrawBG(6, (320 - 32), 35, 1.2f, 1.0f, 1.0f);
+		//Draw copyright text
+		DrawBG(7, 64, (480 - 84), 1.2f, 1.0f, 1.0f);
+		logodrawn = logoframe;
+		if (transitionmode != 0)
+		{
+			if (transitionmode == 1)
+			{
+				{
+					if (LogoScaleXT < LogoScaleX * 2.0f)LogoScaleXT = LogoScaleXT*1.12f;
+					if (LogoScaleYT < LogoScaleY * 2.0f)LogoScaleYT = LogoScaleYT*1.12f;
+				}
+				transitionframe++;
+			}
+			xpos = (320 - LogoScaleXT * 256.0f * HorizontalStretch) / 2.0f;
+			ypos = (240 - LogoScaleYT * 128.0f * HorizontalStretch) / 2.0f;
+			//Draw logo transition
+			SetVtxColorB(0x7FFFFFFF);
+			if (LogoScaleXT <= 2.0f) DrawBG(4, xpos, ypos, 1.2f, LogoScaleXT, LogoScaleYT);
+		}
+	}
+}
+
 
 void DrawPressStart()
 {
@@ -262,10 +349,15 @@ void DrawPressStart()
 	if (float(HorizontalResolution) / float(VerticalResolution) <= 1.3f) yoff = 134; //5:4
 	if (float(HorizontalResolution) / float(VerticalResolution) == 1.5f) yoff = 92.0f; //3:2
 	if (float(HorizontalResolution) / float(VerticalResolution) > 2.2f) yoff = 48.0f; //Ultra wide
-	if (startdrawn != startframe)
+	if ( !(HorizontalResolution == 640 && VerticalResolution == 480) && startdrawn != startframe)
 	{
 		if (startframe > 128) startframe = 0;
 		DrawBG(8, HorizontalStretch*(320.0f - 128.0f+PressStartOffsetX), VerticalResolution - HorizontalStretch*(yoff-PressStartOffsetY), 1.1f, HorizontalStretch * 0.5f, VerticalStretch * rewritestretch);
+	}
+	if (HorizontalResolution == 640 && VerticalResolution == 480 && startdrawn != startframe)
+	{
+		if (startframe > 128) startframe = 0;
+		DrawBG(8, (320.0f - 128.0f), (480 - 114), 1.1f, 0.5f, 0.5f);
 	}
 	startdrawn = startframe;
 }
@@ -339,12 +431,28 @@ extern "C"
 		LogoScaleYT = LogoScaleY;
 		delete settings;
 		//Main code
+		//Kill Cream
+		if (RemoveCream == true) WriteData<1>((void*)0x6353A0, 0xC3u);
 		if (DisableSA1Titlescreen == false)
 		{
-			//Kill Cream
-			if (RemoveCream == true) WriteData((void*)0x6353A0, 0xC3u, 1);
+			//640x480 stuff
+			WriteCall((void*)0x0050E547, DrawTitleBG_640);
+			WriteCall((void*)0x0050E58E, DrawLogo_640);
+			WriteData<5>((void*)0x0050E659, 0x90);
+			WriteData<5>((void*)0x0050E6F4, 0x90);
+			WriteData<5>((void*)0x0050E754, 0x90);
+			WriteData<5>((void*)0x0050E7B4, 0x90);
+			WriteData<5>((void*)0x0050E814, 0x90);
+			WriteData<5>((void*)0x0050EAB1, 0x90);
+			WriteData<5>((void*)0x0050EB04, 0x90);
+			WriteData<5>((void*)0x0050EB57, 0x90);
+			WriteData<5>((void*)0x0050F1C5, 0x90);
+			WriteData<5>((void*)0x0050F570, 0x90);
+			WriteData<5>((void*)0x0050F5C1, 0x90);
+			WriteData<5>((void*)0x0050F612, 0x90);
+			WriteData<5>((void*)0x0050FF59, 0x90);
 			//Kill titlescreen fade
-			if (DisableFade == true) WriteData((char*)0x0050E49B, 0x90, 5);
+			if (DisableFade == true) WriteData<5>((char*)0x0050E49B, 0x90);
 			ResizeTextureList((NJS_TEXLIST*)0x010D7C80, 10);
 			WriteJump((void*)0x505810, sub_505810);
 			WriteJump((void*)0x50BA90, DrawAVA_TITLE_BACK_E_DC);
@@ -391,61 +499,61 @@ extern "C"
 			WriteCall((void*)0x0050E8AF, DrawLogo);
 			WriteCall((void*)0x0051002B, DrawPressStart);
 			//Kill other BG
-			WriteData((char*)0x0050E754, 0x90, 5);
-			WriteData((char*)0x0050E7B4, 0x90, 5);
-			WriteData((char*)0x0050E814, 0x90, 5);
-			WriteData((char*)0x0050EA11, 0x90, 5);
-			WriteData((char*)0x0050E99A, 0x90, 5);
-			WriteData((char*)0x0050E929, 0x90, 5);
+			WriteData<5>((char*)0x0050E754, 0x90);
+			WriteData<5>((char*)0x0050E7B4, 0x90);
+			WriteData<5>((char*)0x0050E814, 0x90);
+			WriteData<5>((char*)0x0050EA11, 0x90);
+			WriteData<5>((char*)0x0050E99A, 0x90);
+			WriteData<5>((char*)0x0050E929, 0x90);
 			//Kill Sonic
-			WriteData((char*)0x0050EC03, 0x90, 5);
-			WriteData((char*)0x0050EC6D, 0x90, 5);
-			WriteData((char*)0x0050ECD7, 0x90, 5);
-			WriteData((char*)0x0050ED4F, 0x90, 5);
-			WriteData((char*)0x0050EDC7, 0x90, 5);
-			WriteData((char*)0x0050EE31, 0x90, 5);
-			WriteData((char*)0x0050EEA9, 0x90, 5);
-			WriteData((char*)0x0050EF21, 0x90, 5);
-			WriteData((char*)0x0050EF99, 0x90, 5);
-			WriteData((char*)0x0050F003, 0x90, 5);
-			WriteData((char*)0x0050F07B, 0x90, 5);
-			WriteData((char*)0x0050F0F3, 0x90, 5);
-			WriteData((char*)0x0050F6BA, 0x90, 5);
-			WriteData((char*)0x0050F722, 0x90, 5);
-			WriteData((char*)0x0050F78A, 0x90, 5);
-			WriteData((char*)0x0050F800, 0x90, 5);
-			WriteData((char*)0x0050F876, 0x90, 5);
-			WriteData((char*)0x0050F8DE, 0x90, 5);
-			WriteData((char*)0x0050F954, 0x90, 5);
-			WriteData((char*)0x0050F9CA, 0x90, 5);
-			WriteData((char*)0x0050FA40, 0x90, 5);
-			WriteData((char*)0x0050FAA8, 0x90, 5);
-			WriteData((char*)0x0050FB1E, 0x90, 5);
-			WriteData((char*)0x0050FB94, 0x90, 5);
-			WriteData((char*)0x0050F16B, 0x90, 5);
-			WriteData((char*)0x0050FC0A, 0x90, 5);
+			WriteData<5>((char*)0x0050EC03, 0x90);
+			WriteData<5>((char*)0x0050EC6D, 0x90);
+			WriteData<5>((char*)0x0050ECD7, 0x90);
+			WriteData<5>((char*)0x0050ED4F, 0x90);
+			WriteData<5>((char*)0x0050EDC7, 0x90);
+			WriteData<5>((char*)0x0050EE31, 0x90);
+			WriteData<5>((char*)0x0050EEA9, 0x90);
+			WriteData<5>((char*)0x0050EF21, 0x90);
+			WriteData<5>((char*)0x0050EF99, 0x90);
+			WriteData<5>((char*)0x0050F003, 0x90);
+			WriteData<5>((char*)0x0050F07B, 0x90);
+			WriteData<5>((char*)0x0050F0F3, 0x90);
+			WriteData<5>((char*)0x0050F6BA, 0x90);
+			WriteData<5>((char*)0x0050F722, 0x90);
+			WriteData<5>((char*)0x0050F78A, 0x90);
+			WriteData<5>((char*)0x0050F800, 0x90);
+			WriteData<5>((char*)0x0050F876, 0x90);
+			WriteData<5>((char*)0x0050F8DE, 0x90);
+			WriteData<5>((char*)0x0050F954, 0x90);
+			WriteData<5>((char*)0x0050F9CA, 0x90);
+			WriteData<5>((char*)0x0050FA40, 0x90);
+			WriteData<5>((char*)0x0050FAA8, 0x90);
+			WriteData<5>((char*)0x0050FB1E, 0x90);
+			WriteData<5>((char*)0x0050FB94, 0x90);
+			WriteData<5>((char*)0x0050F16B, 0x90);
+			WriteData<5>((char*)0x0050FC0A, 0x90);
 			//Kill Logo 1
-			WriteData((char*)0x0050FCAB, 0x90, 5);
-			WriteData((char*)0x0050FD07, 0x90, 5);
-			WriteData((char*)0x0050FD63, 0x90, 5);
-			WriteData((char*)0x0050FDBF, 0x90, 5);
-			WriteData((char*)0x0050FE1B, 0x90, 5);
-			WriteData((char*)0x0050FE85, 0x90, 5);
-			WriteData((char*)0x0050FEEF, 0x90, 5);
+			WriteData<5>((char*)0x0050FCAB, 0x90);
+			WriteData<5>((char*)0x0050FD07, 0x90);
+			WriteData<5>((char*)0x0050FD63, 0x90);
+			WriteData<5>((char*)0x0050FDBF, 0x90);
+			WriteData<5>((char*)0x0050FE1B, 0x90);
+			WriteData<5>((char*)0x0050FE85, 0x90);
+			WriteData<5>((char*)0x0050FEEF, 0x90);
 			//Kill Logo 2
-			WriteData((char*)0x0050F1C5, 0x90, 5);
-			WriteData((char*)0x0050F223, 0x90, 5);
-			WriteData((char*)0x0050F281, 0x90, 5);
-			WriteData((char*)0x0050F2DF, 0x90, 5);
-			WriteData((char*)0x0050F33D, 0x90, 5);
-			WriteData((char*)0x0050F3A9, 0x90, 5);
-			WriteData((char*)0x0050F415, 0x90, 5);
+			WriteData<5>((char*)0x0050F1C5, 0x90);
+			WriteData<5>((char*)0x0050F223, 0x90);
+			WriteData<5>((char*)0x0050F281, 0x90);
+			WriteData<5>((char*)0x0050F2DF, 0x90);
+			WriteData<5>((char*)0x0050F33D, 0x90);
+			WriteData<5>((char*)0x0050F3A9, 0x90);
+			WriteData<5>((char*)0x0050F415, 0x90);
 			//Kill PressEnter
-			WriteData((char*)0x00510085, 0x90, 5);
-			WriteData((char*)0x005100EB, 0x90, 5);
+			WriteData<5>((char*)0x00510085, 0x90);
+			WriteData<5>((char*)0x005100EB, 0x90);
 			//Some other stuff
-			WriteData((char*)0x0050E8BA, 0x1, 1); //Draw logo only once
-			WriteData((char*)0x0050E81F, 0x1, 1); //Draw title screen BG only once)
+			WriteData<1>((char*)0x0050E8BA, 0x1); //Draw logo only once
+			WriteData<1>((char*)0x0050E81F, 0x1); //Draw title screen BG only once)
 			WriteData((float**)0x0050E87C, &rewritestretch); //Disable logo stretching
 			//Scrolling
 			WriteData((float**)0x0050E83B, &zero); //Disable titlescreen scrolling 
@@ -457,11 +565,11 @@ extern "C"
 			WriteData((float**)0x0050E79D, &one); //Disable vertical offset row 3
 			WriteData((float**)0x0050E7FD, &one); //Disable vertical offset row 4
 		}
-		WriteData((void*)0x0042CCF3, 0x0F, 1); //Disable Sonic Team logo
+		WriteData<1>((void*)0x0042CCF3, 0x0F); //Disable Sonic Team logo
 		//Pause box stuff
 		WriteData((float*)0x00458501, 17.1f); //Pause box X scale
-		WriteData((char*)0x00459106, 0x50, 1); //Controls X shift
-		WriteData((char*)0x0045917E, 0x50, 1); //Controls X shift
+		WriteData<1>((char*)0x00459106, 0x50); //Controls X shift
+		WriteData<1>((char*)0x0045917E, 0x50); //Controls X shift
 		WriteData((float**)0x004584C6, &pausexpos); //Pause box X position
 		WriteData((float*)0x00459070, 17.1f); //Controls X scale
 		WriteData((float**)0x00459054, &control_vertoffset);
@@ -495,7 +603,7 @@ extern "C"
 		}
 		if (GameState == 16)
 		{
-		char optionCount = SetPauseDisplayOptions(&options);
+		char optionCount = GetPauseDisplayOptions(&options);
 		if (optionCount == 6)
 		{
 			WriteData((float*)0x0045852C, 21.0f); //Pause box Y scale (4 options)
