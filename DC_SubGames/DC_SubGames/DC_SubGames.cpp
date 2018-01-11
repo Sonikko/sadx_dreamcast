592fde4fbad4a21d47a11fa9b8edb37c350e94a5
PiKeyAr <pikeyar@sadx>
2018-01-11 21:42:29 +0900

    Disable lock-on sprite if paused

--- DC_SubGames/DC_SubGames/DC_SubGames.cpp
+++ DC_SubGames/DC_SubGames/DC_SubGames.cpp
@@ -737,30 +737,34 @@ void __cdecl TornadoTargetSprite_TargetLock_DisplayX(ObjectMaster *a1)
 	EntityData1 *v1; // esi
 	NJS_POINT2 position; // [esp+4h] [ebp-8h]
 
-	v1 = a1->Data1;
-	if (!MissedFrames && !CheckSkyChaseActionThing())
+	v1 = a1->Data1;	if (GameState != 16)
 	{
-		njPushMatrix(0);
-		njColorBlendingMode(0, NJD_COLOR_BLENDING_ONE);
-		njColorBlendingMode(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_ONE);
-		SetMaterialAndSpriteColor_Float(1.0f, 1.0f, 1.0f, 1.0f);
-		njSetTexture(&TARGET_TEXLIST);
-		njProjectScreen(0, &v1->Position, &position);
-		*(NJS_POINT2 *)&TornadoTarget_SPRITE.p.x = position;
-		TornadoTarget_SPRITE.sy = v1->Scale.y * (VerticalResolution / 480.0f);
-		njDrawSprite2D_ForcePriority(&TornadoTarget_SPRITE, 2, 1000.0f, NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR);
-		njPopMatrix(1u);
-		njColorBlendingMode(0, NJD_COLOR_BLENDING_SRCALPHA);
-		njColorBlendingMode(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
-		ClampGlobalColorThing_Thing();
+		if (!MissedFrames && !CheckSkyChaseActionThing())
+		{
+			njPushMatrix(0);
+			njColorBlendingMode(0, NJD_COLOR_BLENDING_ONE);
+			njColorBlendingMode(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_ONE);
+			SetMaterialAndSpriteColor_Float(1.0f, 1.0f, 1.0f, 1.0f);
+			njSetTexture(&TARGET_TEXLIST);
+			njProjectScreen(0, &v1->Position, &position);
+			*(NJS_POINT2 *)&TornadoTarget_SPRITE.p.x = position;
+			TornadoTarget_SPRITE.sy = v1->Scale.y * (VerticalResolution / 480.0f);
+			njTextureShadingMode(1);
+			njDrawSprite2D_ForcePriority(&TornadoTarget_SPRITE, 2, 1000.0f, NJD_SPRITE_ALPHA | NJD_SPRITE_COLOR);
+			njPopMatrix(1u);
+			njColorBlendingMode(0, NJD_COLOR_BLENDING_SRCALPHA);
+			njColorBlendingMode(NJD_DESTINATION_COLOR, NJD_COLOR_BLENDING_INVSRCALPHA);
+			ClampGlobalColorThing_Thing();
+			njTextureShadingMode(2);
+		}
 	}
 }
 
 void TornadoTarget_Render(NJS_SPRITE *sp, Int n, Float pri, NJD_SPRITE attr, QueuedModelFlagsB queue_flags)
 {
-	njTextureShadingMode(1);
-	njDrawSprite2D_Queue(sp, n, pri, attr, queue_flags);
-	njTextureShadingMode(2);
+		njTextureShadingMode(1);
+		njDrawSprite2D_Queue(sp, n, pri, attr, queue_flags);
+		njTextureShadingMode(2);
 }
 
 void RenderSkyChaseRocket(NJS_POINT3COL *a1, int texnum, NJD_DRAW n, QueuedModelFlagsB a4)
